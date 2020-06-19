86329fdec47a247388bac4dc830eaf629a403c2f
Alejandro Sanchez <aalejo@gmail.com>
2020-06-18 23:28:01 -0400

    added my first unit testing

--- breathecode/authenticate/tests.py
+++ breathecode/authenticate/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
-
+from django.apps import apps
+from django.urls.base import reverse_lazy
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User, Group
+from .models import CredentialsGithub
 # Create your tests here.
+
+#@override_settings(STATICFILES_STORAGE=None)
+class AuthenticateTestSuite(TestCase):
+    """
+    Endpoint tests for Invites
+    """
+    def setUp(self):
+        user = mixer.blend('auth.User')
+        user.set_password('pass1234')
+        user.save()
+
+        params = { "user": user }
+        github = mixer.blend('authenticate.CredentialsGithub', **params)
+        github.save()
+
+    def test_get_users(self):
+        url = reverse_lazy('authenticate:user')
+        response = self.client.get(url)
+        users = response.json()
+        
+        # total_users = User.objects.all().count()
+        self.assertEqual(1,len(users),"The total users should match the database")
+
+
+    def test_get_me(self):
+        url = reverse_lazy('authenticate:user_me')
+        response = self.client.get(url)
+        users = response.json()
+        
+        # total_users = User.objects.all().count()
+        self.assertEqual(1,len(users),"I should be able to request my own information using user/me")
--- breathecode/authenticate/urls.py
+++ breathecode/authenticate/urls.py
@@ -22,8 +22,8 @@ from rest_framework.authtoken import views
 
 app_name='authenticate'
 urlpatterns = [
-    path('user/', get_users),
-    path('user/me', get_users_me),
+    path('user/', get_users, name="user"),
+    path('user/me', get_users_me, name="user_me"),
     path('group/', get_groups),
     path('token/', CustomAuthToken.as_view()),
     path('github/', get_github_token),
