8ce68e1d48d7bbd97bcf41741f217b1d1c309847
Alejandro Sanchez <aalejo@gmail.com>
2020-08-07 17:19:22 -0400

    syllabus version

--- breathecode/coursework/serializers.py
+++ breathecode/coursework/serializers.py
@@ -1,7 +1,52 @@
-from .models import Course
+from .models import Course, Syllabus
 from rest_framework import serializers
+import serpy
 
-class GetCourseSerializer(serializers.ModelSerializer):
+class GetCourseSerializer(serpy.Serializer):
+    """The serializer schema definition."""
+    # Use a Field subclass like IntField if you need more validation.
+    id = serpy.Field()
+    slug = serpy.Field()
+    name = serpy.Field()
+    logo = serpy.Field()
+    duration_in_hours = serpy.Field()
+    duration_in_days = serpy.Field()
+    week_hours = serpy.Field()
+    updated_at = serpy.Field()
+    created_at = serpy.Field()
+
+class SyllabusGetSerializer(serpy.Serializer):
+    """The serializer schema definition."""
+    # Use a Field subclass like IntField if you need more validation.
+    version = serpy.Field()
+    course = serpy.MethodField()
+    updated_at = serpy.Field()
+    json = serpy.Field()
+
+    def get_course(self, obj):
+        return obj.course.slug
+
+class SyllabusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Syllabus
+        exclude = ()
+        extra_kwargs = {
+            'course': {'read_only': True},
+        }
+
+    def create(self, validated_data):
+        return super(SyllabusSerializer, self).create({ **validated_data, "course": self.context['course'] })
+
+class SyllabusUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Syllabus
+        exclude = ()
+        extra_kwargs = {
+            'course': {'read_only': True},
+            'version': {'read_only': True},
+        }
+
+class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         exclude = ()
--- breathecode/coursework/urls.py
+++ breathecode/coursework/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import get_courses
+from .views import get_courses, get_single_course, SyllabusView
 from rest_framework.authtoken import views
 
-app_name='events'
+app_name='coursework'
 urlpatterns = [
-    path('/course', get_courses),
+    path('course', get_courses),
+    path('course/<str:course_slug>/', get_single_course),
+    path('course/<str:course_slug>/syllabus', SyllabusView.as_view()),
+    path('course/<str:course_slug>/syllabus/<int:version>', SyllabusView.as_view()),
 ]
 
