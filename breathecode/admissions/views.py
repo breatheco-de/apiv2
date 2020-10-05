594569edf84ab533d7c69a1b8a08adbe45170a8c
Madelene Campos <madelenecampos@gmail.com>
2020-10-05 15:50:46 -0400

    Removes unused imports and fixes flake8 errors

--- breathecode/admissions/views.py
+++ breathecode/admissions/views.py
@@ -1,15 +1,15 @@
 from django.utils import timezone
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from .serializers import (
-    AcademySerializer, CohortSerializer, CertificateSerializer,
+    AcademySerializer, CertificateSerializer,
     GetCohortSerializer, UserSerializer
 )
 from .models import Academy, CohortUser, Certificate, Cohort
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
+
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -24,7 +24,7 @@ def get_cohorts(request):
     academy = request.GET.get('academy', None)
     if academy is not None:
         items = items.filter(academy__slug__in=academy.split(","))
-    
+
     location = request.GET.get('location', None)
     if location is not None:
         items = items.filter(academy__slug__in=academy.split(","))
@@ -32,6 +32,7 @@ def get_cohorts(request):
     items = items.order_by('kickoff_date')
     serializer = GetCohortSerializer(items, many=True)
     return Response(serializer.data)
+
 
 # Create your views here.
 class AcademyView(APIView):
@@ -50,6 +51,7 @@ class AcademyView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class CohortUserView(APIView):
     """
     List all snippets, or create a new snippet.
@@ -58,7 +60,6 @@ class CohortUserView(APIView):
         items = CohortUser.objects.all()
         serializer = UserSerializer(items, many=True)
         return Response(serializer.data)
-
 
 
 class CohortView(APIView):
