70be8098db22795254b06fae1d05bde4859f9d76
Alejandro Sanchez <aalejo@gmail.com>
2020-11-11 17:12:28 +0000

    sync slack commands

---
+++ breathecode/notify/management/commands/sync_messaging.py
@@ -0,0 +1,67 @@
+import os, requests, sys, pytz
+from datetime import datetime
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+from ...models import SlackTeam
+from ...actions import sync_slack_team_users, sync_slack_team_channel
+from breathecode.admissions.models import CohortUser
+from django.db.models import Count
+
+HOST = os.environ.get("OLD_BREATHECODE_API")
+DATETIME_FORMAT="%Y-%m-%d"
+class Command(BaseCommand):
+    help = 'Sync academies from old breathecode'
+
+    def add_arguments(self, parser):
+        parser.add_argument('entity', type=str)
+        parser.add_argument(
+            '--cohorts',
+            type=str,
+            default=None,
+            help='Cohorts slugs to sync',
+        )
+        parser.add_argument(
+            '--students',
+            type=str,
+            default=None,
+            help='Cohorts slugs to sync',
+        )
+        parser.add_argument(
+              '--limit',
+               action='store',
+               dest='limit',
+               type=int,
+               default=0,
+               help='How many to import'
+        )
+
+    def handle(self, *args, **options):
+        try:
+            func = getattr(self,options['entity'],'entity_not_found') 
+        except TypeError:
+            print(f'Sync method for {options["entity"]} no Found!')
+        func(options)
+
+    def slack_users(self, options):
+
+        limit = False
+        total = 0
+        if 'limit' in options and options['limit']:
+            limit = options['limit']
+
+        teams = SlackTeam.objects.all()
+        for team in teams:
+            sync_slack_team_users(team.id)
+
+    def slack_channels(self, options):
+
+        limit = False
+        total = 0
+        if 'limit' in options and options['limit']:
+            limit = options['limit']
+
+        teams = SlackTeam.objects.all()
+        for team in teams:
+            sync_slack_team_channel(team.id)
+
+
