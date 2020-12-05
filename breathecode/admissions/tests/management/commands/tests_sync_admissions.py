73695343fa07659f09646ee0d4780ca2eeb4e8fc
jefer94 <jdefreitaspinto@gmail.com>
2020-12-05 04:12:56 -0500

    add run script twice test

--- breathecode/admissions/tests/management/commands/tests_sync_admissions.py
+++ breathecode/admissions/tests/management/commands/tests_sync_admissions.py
@@ -114,6 +114,70 @@ class AcademyCohortTestSuite(AdmissionsTestCase):
     @patch(GOOGLE_CLOUD_PATH['bucket'], apply_google_cloud_bucket_mock())
     @patch(GOOGLE_CLOUD_PATH['blob'], apply_google_cloud_blob_mock())
     @patch(LEGACY_API_PATH['get'], apply_requests_get_mock())
+    def test_students_twice(self):
+        """Test /academy/cohort without auth"""
+        cohorts = set()
+        count_cohorts = 0
+
+        for student in legacy_students['data']:
+            for cohort in student['cohorts']:
+                cohorts.add(cohort)
+                count_cohorts += 1
+
+        models = [mixer.blend('admissions.Cohort', slug=slug) for slug in cohorts]
+        models_dict = [self.remove_dinamics_fields(model.__dict__) for model in models]
+
+        command = Command()
+        self.assertEqual(self.count_cohort_user(), 0)
+        
+        self.assertEqual(command.students({'override': False}), None)
+        self.assertEqual(command.students({'override': False}), None) # call twice
+        self.assertEqual(self.count_cohort(), len(cohorts))
+        self.assertEqual(self.count_user(), 10)
+        self.assertEqual(self.count_cohort_user(), count_cohorts)
+        self.assertEqual(self.all_cohort_dict(), models_dict)
+
+        cohort_user_acc = 0
+
+        for student in legacy_students['data']:
+            for slug in student['cohorts']:
+                email = student['email']
+                cohort = Cohort.objects.filter(slug=slug).values_list('id', flat=True).first()
+                user = User.objects.filter(email=email).values_list('id', flat=True).first()
+
+                filter = {
+                    'cohort_id': cohort,
+                    'user_id': user,
+                }
+
+                self.assertEqual(CohortUser.objects.filter(**filter).count(), 1)
+
+                model = CohortUser.objects.filter(**filter).first().__dict__
+                del model['_state']
+
+                self.assertEqual(isinstance(model['created_at'], datetime.datetime), True)
+                del model['created_at']
+
+                self.assertEqual(isinstance(model['updated_at'], datetime.datetime), True)
+                del model['updated_at']
+
+                cohort_user_acc += 1
+
+                self.assertEqual(model, {
+                    'id': cohort_user_acc,
+                    'cohort_id': cohort,
+                    'user_id': user,
+                    'educational_status': educational_status[student['status']],
+                    'finantial_status': financial_status[student['financial_status']],
+                    'role': 'STUDENT',
+                })
+
+        self.assertEqual(self.count_cohort_user(), cohort_user_acc)
+
+    @patch(GOOGLE_CLOUD_PATH['client'], apply_google_cloud_client_mock())
+    @patch(GOOGLE_CLOUD_PATH['bucket'], apply_google_cloud_bucket_mock())
+    @patch(GOOGLE_CLOUD_PATH['blob'], apply_google_cloud_blob_mock())
+    @patch(LEGACY_API_PATH['get'], apply_requests_get_mock())
     def test_teachers(self):
         """Test /academy/cohort without auth"""
         cohorts = set()
@@ -131,6 +195,70 @@ class AcademyCohortTestSuite(AdmissionsTestCase):
         self.assertEqual(self.count_cohort_user(), 0)
         
         self.assertEqual(command.teachers({'override': False}), None)
+        self.assertEqual(self.count_cohort(), len(cohorts))
+        self.assertEqual(self.count_user(), 10)
+        self.assertEqual(self.count_cohort_user(), count_cohorts)
+        self.assertEqual(self.all_cohort_dict(), models_dict)
+
+        cohort_user_acc = 0
+
+        for student in legacy_teachers['data']:
+            for slug in student['cohorts']:
+                email = student['username']
+                cohort = Cohort.objects.filter(slug=slug).values_list('id', flat=True).first()
+                user = User.objects.filter(email=email).values_list('id', flat=True).first()
+
+                filter = {
+                    'cohort_id': cohort,
+                    'user_id': user,
+                }
+
+                self.assertEqual(CohortUser.objects.filter(**filter).count(), 1)
+
+                model = CohortUser.objects.filter(**filter).first().__dict__
+                del model['_state']
+
+                self.assertEqual(isinstance(model['created_at'], datetime.datetime), True)
+                del model['created_at']
+
+                self.assertEqual(isinstance(model['updated_at'], datetime.datetime), True)
+                del model['updated_at']
+
+                cohort_user_acc += 1
+
+                self.assertEqual(model, {
+                    'id': cohort_user_acc,
+                    'cohort_id': cohort,
+                    'user_id': user,
+                    'educational_status': None,
+                    'finantial_status': None,
+                    'role': 'TEACHER',
+                })
+
+        self.assertEqual(self.count_cohort_user(), cohort_user_acc)
+
+    @patch(GOOGLE_CLOUD_PATH['client'], apply_google_cloud_client_mock())
+    @patch(GOOGLE_CLOUD_PATH['bucket'], apply_google_cloud_bucket_mock())
+    @patch(GOOGLE_CLOUD_PATH['blob'], apply_google_cloud_blob_mock())
+    @patch(LEGACY_API_PATH['get'], apply_requests_get_mock())
+    def test_teachers_twice(self):
+        """Test /academy/cohort without auth"""
+        cohorts = set()
+        count_cohorts = 0
+
+        for teacher in legacy_teachers['data']:
+            for cohort in teacher['cohorts']:
+                cohorts.add(cohort)
+                count_cohorts += 1
+
+        models = [mixer.blend('admissions.Cohort', slug=slug) for slug in cohorts]
+        models_dict = [self.remove_dinamics_fields(model.__dict__) for model in models]
+
+        command = Command()
+        self.assertEqual(self.count_cohort_user(), 0)
+        
+        self.assertEqual(command.teachers({'override': False}), None)
+        self.assertEqual(command.teachers({'override': False}), None) # call twice
         self.assertEqual(self.count_cohort(), len(cohorts))
         self.assertEqual(self.count_user(), 10)
         self.assertEqual(self.count_cohort_user(), count_cohorts)
