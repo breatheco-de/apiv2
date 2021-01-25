f79907ef5ff929ed24f2fc52db95c87870fcaff4
jefer94 <jdefreitaspinto@gmail.com>
2021-01-25 18:03:29 -0500

    test other cache config

--- breathecode/settings.py
+++ breathecode/settings.py
@@ -269,15 +269,34 @@ CORS_ALLOW_HEADERS = [
 ]
 
 # TODO: configure cache
-REDIS_URL = os.getenv('REDIS_URL', None)
+REDIS_URL = os.getenv('REDIS_URL', '')
 # SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'redis_cache.RedisCache',
+#         'LOCATION': REDIS_URL,
+#         'OPTIONS': {
+#             'DB': 0,
+#         },
+#     },
+# }
 CACHES = {
     'default': {
         'BACKEND': 'redis_cache.RedisCache',
-        'LOCATION': REDIS_URL,
+        'LOCATION': [
+            REDIS_URL
+        ],
         'OPTIONS': {
-            'DB': 0,
+            'DB': 1,
+            'PARSER_CLASS': 'redis.connection.HiredisParser',
+            'CONNECTION_POOL_CLASS': 'redis.BlockingConnectionPool',
+            'CONNECTION_POOL_CLASS_KWARGS': {
+                'max_connections': 50,
+                'timeout': 20,
+            },
+            'MAX_CONNECTIONS': 1000,
+            'PICKLE_VERSION': -1,
         },
     },
 }
