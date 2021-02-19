eb0100685660dd06c6659af40eacda5580608570
Alexson <alexsoncoelho21@hotmail.com>
2021-02-18 21:07:20 -0500

    no capability test

--- breathecode/authenticate/tests/urls/tests_resend_invitation.py
+++ breathecode/authenticate/tests/urls/tests_resend_invitation.py
@@ -8,7 +8,7 @@ from ..mixins import AuthTestCase
 from breathecode.tests.mixins import HeadersMixin, GenerateModelsMixin
 
 
-class AuthenticateTestSuite(AuthTestCase, HeadersMixin, GenerateModelsMixin):
+class AuthenticateTestSuite(AuthTestCase, GenerateModelsMixin):
     """Authentication test suite"""
     def test_resend_invite_no_auth(self):
         """Test """
@@ -25,26 +25,28 @@ class AuthenticateTestSuite(AuthTestCase, HeadersMixin, GenerateModelsMixin):
     def test_resend_invite_no_capability(self):
         """Test """
         self.headers(academy=4)
-        self.headers(capability=4)
-        self.generate_models(authenticate=True)
+        self.generate_models(authenticate=True, profile_academy=True,
+            capability='crud_cohort', role='potato')
 
         url = reverse_lazy('authenticate:academy_resent_invite', kwargs={"user_id":2})
         
         response = self.client.put(url)
         json = response.json()
-        expected = {'detail': "You (user: 2) don't have this [63 chars]", 'status_code': 403} 
+        expected = {'detail': "You (user: 2) don't have this capability: admissions_developer for "
+                    'academy 4','status_code': 403} 
         self.assertEqual(json, expected)
-        self.assertEqual(response.status_code, 405)
+        self.assertEqual(response.status_code, 403)
         
-    # def test_resend_invite_with_capability(self):
-    #     """Test """
-    #     self.headers(academy=4)
-    #     self.generate_models(authenticate=True)
+    def test_resend_invite_with_capability(self):
+        """Test """
+        self.headers(academy=4)
+        self.generate_models(authenticate=True, profile_academy=True,
+            capability='admissions_developer', role='banana')
 
-    #     url = reverse_lazy('authenticate:academy_resent_invite', kwargs={"user_id":1359})
+        url = reverse_lazy('authenticate:academy_resent_invite', kwargs={"user_id":1359})
         
-    #     response = self.client.put(url)
-    #     json = response.json()
-    #     expected = {'detail': "You (user: 2) don't have this[64 chars] 403} != True
-    #     self.assertEqual(json, expected)
-    #     self.assertEqual(response.status_code, 403)
+        response = self.client.put(url)
+        json = response.json()
+        expected = {}
+        self.assertEqual(json, expected)
+        self.assertEqual(response.status_code, 403)
