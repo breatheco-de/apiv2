1f403d717f0138762820c49fd920a88720ef6970
Alejandro Sanchez <aalejo@gmail.com>
2021-04-13 02:00:34 +0000

    fixed redirect when its not a gitpod thing

--- breathecode/registry/views.py
+++ breathecode/registry/views.py
@@ -6,7 +6,7 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import AssetSerializer, AssetBigSerializer
-from breathecode.utils import APIException
+from breathecode.utils import ValidationException
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.views import APIView
@@ -18,9 +18,12 @@ from django.http import HttpResponseRedirect
 def redirect_gitpod(request, asset_slug):
     alias = AssetAlias.objects.filter(slug=asset_slug).first()
     if alias is None:
-        raise exceptions.NotFound(detail="Asset alias not found", code=status.HTTP_404_NOT_FOUND)
+        raise ValidationException("Asset alias not found", status.HTTP_404_NOT_FOUND)
 
-    return HttpResponseRedirect(redirect_to='https://gitpod.io#'+alias.asset.url)
+    if alias.gitpod:
+        return HttpResponseRedirect(redirect_to='https://gitpod.io#'+alias.asset.url)
+    else:
+        return HttpResponseRedirect(redirect_to=alias.asset.url)
 
 # Create your views here.
 class GetAssetView(APIView):
