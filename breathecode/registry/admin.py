91d62287ce90a62e9405db4870be76c16b9b8a5e
Alejandro Sanchez <aalejo@gmail.com>
2021-04-13 02:06:18 +0000

    add or remove gitpod boolean from assets

--- breathecode/registry/admin.py
+++ breathecode/registry/admin.py
@@ -8,14 +8,23 @@ from .models import Asset, AssetTranslation, AssetTechnology, AssetAlias
 
 logger = logging.getLogger(__name__)
 
+def add_gitpod(modeladmin, request, queryset):
+    assets = queryset.update(gitpod=True)
+add_gitpod.short_description = "Add GITPOD"
+
+def remove_gitpod(modeladmin, request, queryset):
+    assets = queryset.update(gitpod=False)
+remove_gitpod.short_description = "Remove GITPOD"
 # Register your models here.
 @admin.register(Asset)
 class AssetAdmin(admin.ModelAdmin):
     search_fields = ['title', 'slug', 'user__email', 'cohort__slug']
     list_display = ('slug', 'title', 'lang', 'asset_type', 'url_path')
     list_filter = ['asset_type', 'lang']
+    actions = [add_gitpod, remove_gitpod]
     def url_path(self,obj):
         return format_html(f"<a rel='noopener noreferrer' target='_blank' href='{obj.url}'>open</a>")
+
 # Register your models here.
 @admin.register(AssetTranslation)
 class AssetTranslationsAdmin(admin.ModelAdmin):
