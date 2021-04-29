13ca588591ab973d855479e644f8380b342aa1cb
Alejandro Sanchez <aalejo@gmail.com>
2021-04-29 17:23:16 -0400

    Update views.py

--- breathecode/certificate/views.py
+++ breathecode/certificate/views.py
@@ -120,7 +120,7 @@ class CertificateAcademyView(APIView, HeaderLimitOffsetPagination):
         items = UserSpecialty.objects.filter(cohort__academy__id=academy_id)
 
         like = request.GET.get('like', None)
-        if like is not None:
+        if like is not None and like != "":
             items = items.filter(Q(user__profileacademy__first_name__icontains=like) | Q(
                 user__profileacademy__last_name__icontains=like) | Q(user__profileacademy__email__icontains=like))
 
