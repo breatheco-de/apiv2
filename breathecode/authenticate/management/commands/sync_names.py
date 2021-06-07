909e62c888db8b7149f45668f401a6c689cb0fc2
Alejandro Sanchez <aalejo@gmail.com>
2021-06-07 16:43:26 +0000

    User none in the sync_names script

--- breathecode/authenticate/management/commands/sync_names.py
+++ breathecode/authenticate/management/commands/sync_names.py
@@ -18,11 +18,11 @@ class Command(BaseCommand):
         save = False
         for profile in empty_profiles:
             if profile.first_name is None or profile.first_name == "":
-                if profile.user.first_name is not None and profile.user.first_name != "":
+                if profile.user is not None and profile.user.first_name is not None and profile.user.first_name != "":
                     save = True
                     profile.first_name = profile.user.first_name
             if profile.last_name is None or profile.last_name == "":
-                if profile.user.last_name is not None and profile.user.last_name != "":
+                if profile.user is not None and profile.user.last_name is not None and profile.user.last_name != "":
                     save = True
                     profile.last_name = profile.user.last_name
             if save:
@@ -31,11 +31,11 @@ class Command(BaseCommand):
         profiles = ProfileAcademy.objects.filter(Q(first_name__isnull=False), Q(user__first_name__isnull=True) | Q(user__first_name=""))
         print(f"Found {str(profiles.count())} users out of sync")
         for p in profiles:
-            if p.user.first_name is None or p.user.first_name == "":
+            if p.user is not None and p.user.first_name is None or p.user.first_name == "":
                 if p.first_name is not None and p.first_name != "":
                     save = True
                     p.user.first_name = p.first_name
-            if p.user.last_name is None or p.user.last_name == "":
+            if p.user is not None and p.user.last_name is None or p.user.last_name == "":
                 if p.last_name is not None and p.last_name != "":
                     save = True
                     p.user.last_name = p.last_name
