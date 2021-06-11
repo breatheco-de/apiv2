857d0d98a42262bd56c315ddfe68b8ac42b6bb7a
99lalo <74028591+99lalo@users.noreply.github.com>
2021-06-10 22:05:54 -0400

    Last things

--- breathecode/utils/admin_timezone.py
+++ breathecode/utils/admin_timezone.py
@@ -1,10 +1,11 @@
 from django.utils import timezone
-import pytz
+import pytz, os
 
 class TimezoneMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        timezone.activate(pytz.timezone("America/New_York"))
+        if ENV != "test":
+            timezone.activate(pytz.timezone("America/New_York"))
         return self.get_response(request)
