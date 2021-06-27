d6906021aa8af63a2a82c92236abac1146ce3080
jefer94 <jdefreitaspinto@gmail.com>
2021-06-27 02:43:51 -0500

    fix docker image

---
+++ scripts/docker_entrypoint.py
@@ -0,0 +1,31 @@
+#!/bin/env python
+
+from __future__ import absolute_import
+
+import os
+import sys
+
+
+def execute(command):
+    exit_code = os.system(command)
+
+    if exit_code:
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    print('')
+    print('Collect statics')
+    execute('python manage.py collectstatic --noinput')
+
+    print('')
+    print('Migrate')
+    execute('python manage.py migrate')
+
+    print('')
+    print('Load fixtures')
+    execute('python manage.py loaddata breathecode/*/fixtures/dev_*.json')
+
+    print('')
+    print('Run server')
+    execute('gunicorn --bind :8000 --workers 3 breathecode.wsgi:application')
