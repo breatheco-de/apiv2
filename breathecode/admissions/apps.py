c666a5ca91718b7c3bdad9ff1db05fb4b39e0d8a
jefer94 <jdefreitaspinto@gmail.com>
2021-07-01 11:09:58 -0500

    adding a bad line to test if precommit work

--- breathecode/admissions/apps.py
+++ breathecode/admissions/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-
+a = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,]
 class AcademyConfig(AppConfig):
     name = 'breathecode.admissions'
