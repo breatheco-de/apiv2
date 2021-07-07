332bb120939512bd163be45a5ecfc73fc8607e4b
Alejandro Sanchez <aalejo@gmail.com>
2021-07-07 02:21:09 +0000

    implemented invite_accepted signal to allow other apps to recieve it, added receiver on marketing app

--- breathecode/marketing/receivers.py
+++ breathecode/marketing/receivers.py
@@ -1,14 +1,18 @@
+import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from breathecode.authenticate.signals import invite_accepted
 from breathecode.authenticate.models import ProfileAcademy
 from .models import FormEntry
 
-@receiver(post_save, sender=ProfileAcademy)
-def post_save_profileacademy(sender, instance, created, raw, update_fields, **kwargs):
+logger = logging.getLogger(__name__)
+
+@receiver(invite_accepted, sender=ProfileAcademy)
+def post_save_profileacademy(sender, instance, **kwargs):
     # if a new ProfileAcademy is created on the authanticate app
-    if raw == False and update_fields is not None and "user" in update_fields and update_fields["user"] is not None:
-        # look for the email on the formentry list and bind it
-        entries = FormEntry.objects.filter(email=instance.user.email, user__isnull=True)
-        for entry in entries:
-            entry.user = instance.user
-            entry.save()
+    # logger.debug("invite_accepted", instance)
+    # look for the email on the formentry list and bind it
+    entries = FormEntry.objects.filter(email=instance.user.email, user__isnull=True)
+    for entry in entries:
+        entry.user = instance.user
+        entry.save()
