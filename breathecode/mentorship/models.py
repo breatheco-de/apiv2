fafe02a936e4101d31bc0c03202166ce8b50863d
Alejandro Sanchez <aalejo@gmail.com>
2021-07-16 22:16:38 +0000

    Revert "google calendar integration"
    This reverts commit 6be3edf7a84c26aedab1131a0c71d16842f5f672.

--- breathecode/mentorship/models.py
+++ breathecode/mentorship/models.py
@@ -1,30 +1,19 @@
 import re
 from breathecode.admissions.models import Academy
-from breathecode.authenticate.models import ProfileAcademy
 from django.contrib.auth.models import User
 from django.db import models
 from slugify import slugify
 
 
-class MentorshipService(models.Model):
-    slug = models.SlugField(max_length=150, unique=True)
-    name = models.CharField(max_length=150)
-    description = models.TextField(max_length=450)
+# class MentorProfile(models.Model):
+#     slug = models.SlugField(max_length=150, unique=True)
+#     name = models.CharField(max_length=150)
 
-    price_per_hour = models.FloatField()
-    
-    academy = models.ForeignKey(Academy, on_delete=models.CASCADE, blank=True, null=True)
+#     created_at = models.DateTimeField(auto_now_add=True, editable=False)
+#     updated_at = models.DateTimeField(auto_now=True, editable=False)
 
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(auto_now=True, editable=False)
-
-    def __str__(self):
-        return f'{self.name} ({self.id})'
-
-class MentorAvailableTimeSlot(TimeSlot):
-    
-    mentor = models.ForeignKey(ProfileAcademy, on_delete=models.CASCADE)
-    service = models.ForeignKey(MentorshipService, on_delete=models.CASCADE, blank=True, null=True, default=None, help_text="The availability can be constrained to one particular service (or not)")
+#     def __str__(self):
+#         return f'{self.name} ({self.id})'
 
 # class MentorshipSession(models.Model):
 #     slug = models.SlugField(max_length=150, unique=True)
