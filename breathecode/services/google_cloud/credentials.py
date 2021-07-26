096731107155b4d333a354d81dc9e152f8f6843a
jefer94 <jdefreitaspinto@gmail.com>
2021-07-26 13:56:24 -0500

    test credentials and report errors with the environment

--- breathecode/services/google_cloud/credentials.py
+++ breathecode/services/google_cloud/credentials.py
@@ -2,17 +2,26 @@
 Google Cloud Credentials
 """
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def resolve_credentials():
-    """Resolve Google Cloud Credentials"""
+    """Resolve Google Cloud credentials, returns True if is successfully"""
     path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS', '')
+    if not path:
+        logger.error('GOOGLE_APPLICATION_CREDENTIALS is not set')
+        return False
 
     if os.path.exists(path):
         return True
 
     credentials = os.getenv('GOOGLE_SERVICE_KEY', None)
-    # skip open in development environment
-    if credentials and os.getenv('ENV') != 'development':
-        with open(path, 'w') as credentials_file:
-            credentials_file.write(credentials)
+    if not credentials:
+        logger.error('GOOGLE_SERVICE_KEY is not set')
+        return False
+
+    with open(path, 'w') as credentials_file:
+        credentials_file.write(credentials)
+        return True
