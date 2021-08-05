b96e4ad6b962745eb223dcf32838bae1b2ca3089
Alejandro Sanchez <aalejo@gmail.com>
2021-08-05 06:59:19 +0000

    upgrading monitor slack integation

--- breathecode/monitoring/scripts/monitor_slack_integration.py
+++ breathecode/monitoring/scripts/monitor_slack_integration.py
@@ -5,16 +5,21 @@ Alert me when something is missing with the Slack integration
 from breathecode.notify.models import SlackTeam
 from breathecode.authenticate.models import CredentialsSlack
 from django.db.models import Q
+from breathecode.services.slack import client
 from breathecode.utils import ScriptNotification
 
 slack = SlackTeam.objects.filter(academy__id=academy.id).first()
 if slack is None:
-    raise ScriptNotification('No slack integration has been found',
+    raise ScriptNotification(title='No slack integration has been found',
                              status='MINOR')
 
 owner_credentials = CredentialsSlack.objects.filter(
     user__id=slack.owner.id).first()
 if owner_credentials is None:
     raise ScriptNotification(
-        'The academy slack integration is not finished, the team owner needs to connect with slack',
+        title='The academy slack integration is not finished, the team owner needs to connect with slack',
         status='MINOR')
+
+api = client.Slack(token)
+data = api.post('api.test')
+print("data", data)
