e33183706663561b73916ead1675b1950ab6e09a
Alejandro Sanchez <aalejo@gmail.com>
2021-08-12 23:29:36 +0000

    fixed academy test

--- breathecode/admissions/tests/urls/tests_academy.py
+++ breathecode/admissions/tests/urls/tests_academy.py
@@ -22,7 +22,7 @@ class academyTestSuite(AdmissionsTestCase):
         response = self.client.get(url)
         json = response.json()
 
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_academy_without_data(self):
         """Test /academy without auth"""
