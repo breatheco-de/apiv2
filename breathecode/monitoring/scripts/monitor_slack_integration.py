1e376ef4c3fe8ac54652578e02334ccad2bc1891
Alexson <alexsoncoelho21@hotmail.com>
2021-08-13 16:07:16 -0400

    added pagination datastore and github username

--- breathecode/monitoring/scripts/monitor_slack_integration.py
+++ breathecode/monitoring/scripts/monitor_slack_integration.py
@@ -10,7 +10,8 @@ from breathecode.utils import ScriptNotification
 
 slack = SlackTeam.objects.filter(academy__id=academy.id).first()
 if slack is None:
-    raise ScriptNotification(f'No slack integration has been found for academy={academy.id}',
+    raise ScriptNotification(
+        f'No slack integration has been found for academy={academy.id}',
         title='No slack integration has been found',
         status='MINOR')
 
@@ -19,9 +20,10 @@ owner_credentials = CredentialsSlack.objects.filter(
 if owner_credentials is None:
     raise ScriptNotification(
         'The academy slack integration is not finished, the team owner needs to connect with slack',
-        title='The academy slack integration is not finished, the team owner needs to connect with slack',
+        title=
+        'The academy slack integration is not finished, the team owner needs to connect with slack',
         status='MINOR')
 
 api = client.Slack(owner_credentials.token)
 data = api.post('api.test')
-print("data", data)
+print('data', data)
