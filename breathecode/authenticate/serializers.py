437123f8d647132dc2e9526d24a18b7715153fba
Alejandro Sanchez <aalejo@gmail.com>
2022-01-25 19:44:59 -0500

    Update serializers.py

--- breathecode/authenticate/serializers.py
+++ breathecode/authenticate/serializers.py
@@ -554,7 +554,7 @@ class UserInviteWaitingListSerializer(serializers.ModelSerializer):
             raise ValidationException('Email is required', slug='without-email')
 
         if UserInvite.objects.filter(email=data['email'], status='WAITING_LIST').exists():
-            raise ValidationException('User already exists in the reading list', slug='')
+            raise ValidationException('User already exists in the waiting list', slug='')
 
         data['status'] = 'WAITING_LIST'
 
