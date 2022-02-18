2c781b2b76d8fb8a25e1e19d6a46a39a7f1a94e8
msusanrbrown <sbrow120@fau.edu>
2022-02-18 23:47:59 +0000

    changes to decorator

--- breathecode/services/slack/decorator.py
+++ breathecode/services/slack/decorator.py
@@ -6,6 +6,38 @@ from breathecode.authenticate.models import ProfileAcademy
 logger = logging.getLogger(__name__)
 
 
+def command1(capable_of=None):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+
+            if 'context' not in kwargs or kwargs['context'] is None:
+                raise Exception('Missing scope information on slack command')
+            context = kwargs['context']
+
+            profiles = None
+            if capable_of is not None:
+                profiles = ProfileAcademy.objects.filter(
+                    user__slackuser__slack_id=context['user_id'],
+                    academy__slackteam__slack_id=context['team_id']).values_list('academy__id', flat=True)
+                if len(profiles) == 0:
+                    raise Exception(
+                        f"Your user {context['user_id']} don't have permissions to query this student, are you a staff on this academy?"
+                    )
+
+            kwargs['academies'] = profiles
+            kwargs['user_id'] = context['user_id']
+            kwargs['team_id'] = context['team_id']
+            kwargs['channel_id'] = context['channel_id']
+            kwargs['text'] = context['text']
+
+            result = function(*args, **kwargs)
+            return result
+
+        return wrapper
+
+    return decorator
+
+
 def command(only=None):
     def decorator(function):
         def wrapper(*args, **kwargs):
