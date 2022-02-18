94402649bb64077542437fd1b986f6f8d5576ae9
jefer94 <jdefreitaspinto@gmail.com>
2022-02-18 14:44:36 -0500

    change capability

--- breathecode/authenticate/tests/urls/tests_resend_invitation.py
+++ breathecode/authenticate/tests/urls/tests_resend_invitation.py
@@ -38,17 +38,13 @@ class AuthenticateTestSuite(AuthTestCase):
     def test_resend_invite_no_capability(self):
         """Test """
         self.headers(academy=1)
-        model = self.generate_models(authenticate=True,
-                                     profile_academy=True,
-                                     capability='crud_cohort',
-                                     role='potato',
-                                     syllabus=True)
+        model = self.generate_models(authenticate=True, profile_academy=True, syllabus=True)
         url = reverse_lazy('authenticate:academy_resent_invite', kwargs={'pa_id': 1})
 
         response = self.client.put(url)
         json = response.json()
         expected = {
-            'detail': "You (user: 1) don't have this capability: crud_member for "
+            'detail': "You (user: 1) don't have this capability: invite_resend for "
             'academy 1',
             'status_code': 403
         }
@@ -63,7 +59,7 @@ class AuthenticateTestSuite(AuthTestCase):
         self.headers(academy=1)
         model = self.generate_models(authenticate=True,
                                      profile_academy=True,
-                                     capability='crud_member',
+                                     capability='invite_resend',
                                      role='potato',
                                      syllabus=True)
 
@@ -83,7 +79,7 @@ class AuthenticateTestSuite(AuthTestCase):
         self.headers(academy=1)
         model = self.generate_models(authenticate=True,
                                      profile_academy=True,
-                                     capability='crud_member',
+                                     capability='invite_resend',
                                      role='potato',
                                      syllabus=True)
         url = reverse_lazy('authenticate:academy_resent_invite', kwargs={'pa_id': 1})
@@ -108,7 +104,7 @@ class AuthenticateTestSuite(AuthTestCase):
         user_invite_kwargs = {'email': 'email@dotdotdotdot.dot'}
         model = self.generate_models(authenticate=True,
                                      profile_academy=True,
-                                     capability='crud_member',
+                                     capability='invite_resend',
                                      role='potato',
                                      syllabus=True,
                                      user_invite=True,
@@ -158,7 +154,7 @@ class AuthenticateTestSuite(AuthTestCase):
         past_time = timezone.now() - timedelta(seconds=100)
         model = self.generate_models(authenticate=True,
                                      profile_academy=True,
-                                     capability='crud_member',
+                                     capability='invite_resend',
                                      role='potato',
                                      syllabus=True,
                                      user_invite=True,
