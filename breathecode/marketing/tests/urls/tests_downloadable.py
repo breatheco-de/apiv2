5e54dc160e1e9bda9f83cf562703926c3e11a88e
brcre001 <brandon.f.creed@gmail.com>
2022-04-15 23:49:15 +0000

    added tests for GET requests for marketing/downloadable and marketing/downloadable/:slug

--- breathecode/marketing/tests/urls/tests_downloadable.py
+++ breathecode/marketing/tests/urls/tests_downloadable.py
@@ -9,16 +9,6 @@ from ..mixins import MarketingTestCase
 
 class DownloadableTestSuite(MarketingTestCase):
     """Test /downloadable"""
-
-    # TEST SHOULD COVER THE FOLLOWING:
-
-    # - GET all downloadables: /marketing/downloadable
-    # - GET single downloadable /marketing/downloadable/
-    # - Redirect to single downloadable, same as above but add the?raw=true,
-    # you should be redirected to the downloadable.destination_url
-    # - Update the POST /marketing/lead test to make sure that the current_download
-    # field is being property sent to activecampaign like all the other fields.
-
     def test_downloadable_without_model(self):
         """Test /downloadable to check if it returns an empty list"""
         url = reverse_lazy('marketing:downloadable')
@@ -30,7 +20,7 @@ class DownloadableTestSuite(MarketingTestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(self.all_form_entry_dict(), [])
 
-    def test_downloadable_with_model(self):
+    def test_downloadable_with_data(self):
         """Test /downloadable to check if it returns data after creating model"""
         url = reverse_lazy('marketing:downloadable')
         model = self.generate_models(downloadable=True)
@@ -52,8 +42,17 @@ class DownloadableTestSuite(MarketingTestCase):
         model = self.generate_models(downloadable=2)
         response = self.client.get(url)
         json = response.json()
-        expected = []
+        expected = [{
+            'destination_url': model['downloadable'][0].destination_url,
+            'name': model['downloadable'][0].name,
+            'preview_url': model['downloadable'][0].preview_url,
+            'slug': model['downloadable'][0].slug,
+        }, {
+            'destination_url': model['downloadable'][1].destination_url,
+            'name': model['downloadable'][1].name,
+            'preview_url': model['downloadable'][1].preview_url,
+            'slug': model['downloadable'][1].slug,
+        }]
 
         self.assertEqual(json, expected)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        assert False
---
+++ breathecode/marketing/tests/urls/tests_downloadable_slug.py
@@ -0,0 +1,50 @@
+"""
+Test /downloadable
+"""
+from django.urls.base import reverse_lazy
+from rest_framework import status
+from breathecode.marketing.models import Downloadable
+from ..mixins import MarketingTestCase
+
+
+class DownloadableTestSuite(MarketingTestCase):
+    """Test /downloadable"""
+    def test_downloadable_slug_without_data(self):
+        """Test /downloadable to check if it returns an empty list"""
+        url = reverse_lazy('marketing:single_downloadable', kwargs={'slug': 'test'})
+        response = self.client.get(url)
+        json = response.json()
+        expected = {'detail': 'Document not found', 'status_code': 500}
+
+        self.assertEqual(json, expected)
+        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+        self.assertEqual(self.all_form_entry_dict(), [])
+
+    def test_downloadable_slug_with_data(self):
+        """Test /downloadable to check if it returns an empty list"""
+        model = self.generate_models(downloadable=True)
+        url = reverse_lazy('marketing:single_downloadable', kwargs={'slug': f'{model["downloadable"].slug}'})
+        response = self.client.get(url)
+        json = response.json()
+        expected = {
+            'slug': f'{model["downloadable"].slug}',
+            'name': f'{model["downloadable"].name}',
+            'destination_url': f'{model["downloadable"].destination_url}',
+            'preview_url': f'{model["downloadable"].preview_url}'
+        }
+
+        self.assertEqual(json, expected)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.all_form_entry_dict(), [])
+
+    def test_downloadable_slug_with_data_with_redirect(self):
+        """Test /downloadable to check if it returns an empty list"""
+        model = self.generate_models(downloadable=True)
+        url = reverse_lazy('marketing:single_downloadable', kwargs={'slug': f'{model["downloadable"].slug}'})
+        response = self.client.get(url + '?raw=true')
+        # dir(response) use to see other functions of response
+        expected = model['downloadable'].destination_url
+
+        self.assertEqual(response.url, expected)
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(self.all_form_entry_dict(), [])
