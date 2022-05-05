4aeeaad0e65a95a423ee518a9b611d24a13e022f
Alejandro Sanchez <aalejo@gmail.com>
2022-05-05 23:47:31 +0000

    fix bug when creating a new asset

--- breathecode/registry/admin.py
+++ breathecode/registry/admin.py
@@ -216,7 +216,7 @@ class AssetAdmin(admin.ModelAdmin):
 
     def get_form(self, request, obj=None, **kwargs):
 
-        if 'ipynb' in obj.url:
+        if obj is not None and 'ipynb' in obj.url and len(self.readme) > 2000:
             self.exclude = ('readme', 'html')
         form = super(AssetAdmin, self).get_form(request, obj, **kwargs)
         return form
