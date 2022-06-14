d76d1a3e18b87be4d14fd1a6a4782e314fe2af69
jefer94 <jdefreitaspinto@gmail.com>
2022-06-14 06:06:46 -0500

    add command rerun_eventbrinte_order_placed

---
+++ breathecode/events/management/commands/rerun_eventbrinte_order_placed.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from ...models import EventbriteWebhook
+import breathecode.events.tasks as tasks
+
+
+class Command(BaseCommand):
+    help = 'Sync all the EventbriteWebhook of type order.placed'
+
+    def handle(self, *args, **options):
+        for element in EventbriteWebhook.objects.filter(action='order.placed'):
+            tasks.async_eventbrite_webhook.delay(element.id)
