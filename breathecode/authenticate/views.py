c004b038e11bf56febcedffca0bfdf1b7895b052
jefer94 <jdefreitaspinto@gmail.com>
2022-08-15 02:23:15 -0500

    change the headers

--- breathecode/authenticate/views.py
+++ breathecode/authenticate/views.py
@@ -1956,7 +1956,7 @@ class ProfileMePictureView(APIView):
             logger.info(1111111111)
             logger.info(1111111111)
             if res.headers['Content-Type'].startswith('text/html'):
-                return Response(res.content, headers=res.headers)
+                return Response(res.content, headers={'Content-Type': res.headers['Content-Type']})
 
             json = res.json()
 
