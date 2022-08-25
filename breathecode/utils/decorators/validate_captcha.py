c8c840ddd9a66f4dd801603049b53b08989e4ee1
gustavomm19 <gustavomm199@gmail.com>
2022-08-25 19:00:16 +0000

    doing decoretor wrapper

--- breathecode/utils/decorators/validate_captcha.py
+++ breathecode/utils/decorators/validate_captcha.py
@@ -1,3 +1,4 @@
+import os
 from breathecode.services.google_cloud import Recaptcha
 from breathecode.utils.exceptions import ProgramingError
 from ..validation_exception import ValidationException
@@ -5,20 +6,23 @@ from ..validation_exception import ValidationException
 __all__ = ['validate_captcha']
 
 
-def validate_captcha(request):
+def validate_captcha(function):
 
-    def decorator(function):
+    def wrapper(*args, **kwargs):
+        print('decorator')
+        print('args')
+        print(args[0])
+        try:
+            data = args[0].data.copy()
 
-        def wrapper(*args, **kwargs):
+            project_id = os.getenv('GOOGLE_PROJECT_ID', '')
+            site_key = os.getenv('GOOGLE_CAPTCHA_KEY', '')
+            token = data['token'] if 'token' in data else None
+            action = data['action'] if 'action' in data else None
 
-            try:
-                print(args)
+        except IndexError:
+            raise ProgramingError('Missing request information, use this decorator with DRF View')
 
-            except IndexError:
-                raise ProgramingError('Missing request information, use this decorator with DRF View')
+        return function(*args, **kwargs)
 
-            return function(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
+    return wrapper
