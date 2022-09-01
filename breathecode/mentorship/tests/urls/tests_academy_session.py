9339e0d5ae9728f720683dfc8533348b37a61e69
jefer94 <jdefreitaspinto@gmail.com>
2022-09-01 04:28:11 -0500

    tests changes

--- breathecode/mentorship/tests/urls/tests_academy_session.py
+++ breathecode/mentorship/tests/urls/tests_academy_session.py
@@ -6,6 +6,7 @@ import random
 from unittest.mock import MagicMock, call, patch
 from django.urls.base import reverse_lazy
 from rest_framework import status
+from breathecode.mentorship import signals
 
 from breathecode.utils.api_view_extensions.api_view_extension_handlers import APIViewExtensionHandlers
 from ..mixins import MentorshipTestCase
@@ -120,6 +121,35 @@ def post_serializer(data={}):
         'name': None,
         'online_meeting_url': None,
         'online_recording_url': None,
+        'started_at': None,
+        'starts_at': None,
+        'status': 'PENDING',
+        'summary': None,
+        **data,
+    }
+
+
+def put_serializer(data={}):
+    return {
+        'accounted_duration': None,
+        'agenda': None,
+        'allow_billing': True,
+        'bill': None,
+        'ended_at': None,
+        'ends_at': None,
+        'id': 0,
+        'is_online': False,
+        'latitude': None,
+        'longitude': None,
+        'mentee': 0,
+        'mentee_left_at': None,
+        'mentor': 0,
+        'mentor_joined_at': None,
+        'mentor_left_at': None,
+        'name': None,
+        'online_meeting_url': None,
+        'online_recording_url': None,
+        'service': 0,
         'started_at': None,
         'starts_at': None,
         'status': 'PENDING',
@@ -1119,3 +1149,430 @@ class AcademyServiceTestSuite(MentorshipTestCase):
                 'ends_at': ends_at,
             }),
         ])
+
+    """
+    🔽🔽🔽 PUT capability
+    """
+
+    def test__put__without_capabilities(self):
+        model = self.bc.database.create(user=1)
+
+        self.bc.request.set_headers(academy=1)
+        self.bc.request.authenticate(model.user)
+
+        url = reverse_lazy('mentorship:academy_session')
+        response = self.client.put(url)
+
+        json = response.json()
+        expected = {
+            'detail': "You (user: 1) don't have this capability: crud_mentorship_session for academy 1",
+            'status_code': 403,
+        }
+
+        self.assertEqual(json, expected)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    """
+    🔽🔽🔽 PUT without id
+    """
+
+    @patch('breathecode.mentorship.signals.mentorship_session_status.send', MagicMock())
+    def test__put__without_id(self):
+        model = self.bc.database.create(user=1,
+                                        role=1,
+                                        capability='crud_mentorship_session',
+                                        profile_academy=1)
+
+        self.bc.request.set_headers(academy=1)
+        self.bc.request.authenticate(model.user)
+
+        url = reverse_lazy('mentorship:academy_session')
+        data = [{}]
+        response = self.client.put(url, data, format='json')
+
+        json = response.json()
+        expected = {'detail': 'without-id', 'status_code': 400}
+
+        self.assertEqual(json, expected)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(self.bc.database.list_of('mentorship.MentorshipSession'), [])
+        self.assertEqual(self.bc.database.list_of('mentorship.MentorshipBill'), [])
+
+    """
+    🔽🔽🔽 PUT not found the MentorshipSession
+    """
+
+    @patch('breathecode.mentorship.signals.mentorship_session_status.send', MagicMock())
+    def test__put__not_found(self):
+        cases = [
+            (1, {}, False),
+            (2, {
+                'mentorship_session': 1
+            }, True),
+        ]
+        for id, kwargs, has_instance_db in cases:
+            model = self.bc.database.create(user=1,
+                                            role=1,
+                                            capability='crud_mentorship_session',
+                                            profile_academy=1,
+                                            **kwargs)
+
+            self.bc.request.set_headers(academy=id)
+            self.bc.request.authenticate(model.user)
+
+            url = reverse_lazy('mentorship:academy_session')
+            data = [{'id': id}]
+            response = self.client.put(url, data, format='json')
+
+            json = response.json()
+            expected = {'detail': 'not-found', 'status_code': 404}
+
+            self.assertEqual(json, expected)
+            self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+            self.assertEqual(self.bc.database.list_of('mentorship.MentorshipSession'), [
+                self.bc.format.to_dict(model.mentorship_session),
+            ] if has_instance_db else [])
+            self.assertEqual(self.bc.database.list_of('mentorship.MentorshipBill'), [])
+
+            # teardown
+            self.bc.database.delete('mentorship.MentorshipSession')
+
+    """
+    🔽🔽🔽 PUT found a MentorshipSession, with one MentorProfile and MentorshipService
+    """
+
+    @patch('breathecode.mentorship.signals.mentorship_session_status.send', MagicMock())
+    def test__put__found__without_required_fields(self):
+        model = self.bc.database.create(user=1,
+                                        role=1,
+                                        capability='crud_mentorship_session',
+                                        mentorship_session=1,
+                                        mentorship_service=1,
+                                        mentor_profile=1,
+                                        profile_academy=1)
+
+        self.bc.request.set_headers(academy=1)
+        self.bc.request.authenticate(model.user)
+
+        url = reverse_lazy('mentorship:academy_session')
+        data = [{'id': 1}]
+        response = self.client.put(url, data, format='json')
+
+        json = response.json()
+        expected = [{'mentor': ['This field is required.']}]
+
+        self.assertEqual(json, expected)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(self.bc.database.list_of('mentorship.MentorshipSession'), [
+            self.bc.format.to_dict(model.mentorship_session),
+        ])
+        self.assertEqual(self.bc.database.list_of('mentorship.MentorshipBill'), [])
+
+    @patch('breathecode.mentorship.signals.mentorship_session_status.send', MagicMock())
+    def test__put__found__with_required_fields(self):
+        model = self.bc.database.create(user=1,
+                                        role=1,
+                                        capability='crud_mentorship_session',
+                                        mentorship_session=1,
+                                        mentorship_service=1,
+                                        mentor_profile=1,
+                                        profile_academy=1)
+
+        self.bc.request.set_headers(academy=1)
+        self.bc.request.authenticate(model.user)
+
+        url = reverse_lazy('mentorship:academy_session')
+        data = [{'id': 1, 'mentor': 1}]
+        response = self.client.put(url, data, format='json')
+
+        json = response.json()
+        expected = [put_serializer({
+            'id': 1,
+            'mentee': 1,
+            'service': 1,
+            **data[0],
+        })]
+
+        self.assertEqual(json, expected)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.bc.database.list_of('mentorship.MentorshipSession'), [
+            self.bc.format.to_dict(model.mentorship_session),
+        ])
+        self.assertEqual(self.bc.database.list_of('mentorship.MentorshipBill'), [])
+
+    """
+    🔽🔽🔽 PUT with all required fields, is_online is False
+    """
+
+    @patch('breathecode.mentorship.signals.mentorship_session_status.send', MagicMock())
+    def test__put__found__with_all_required_fields__is_online_as_false(self):
+        mentorship_bill = {'status': random.choice(['RECALCULATE', 'DUE'])}
+        statuses = ['PENDING', 'STARTED', 'COMPLETED', 'FAILED', 'IGNORED']
+        current_status = random.choice(statuses)
+        statuses.remove(current_status)
+        mentorship_session = {'status': current_status}
+
+        model = self.bc.database.create(user=1,
+                                        role=1,
+                                        capability='crud_mentorship_session',
+                                        mentorship_session=mentorship_session,
+                                        mentorship_service=1,
+                                        mentorship_bill=mentorship_bill,
+                                        mentor_profile=1,
+                                        profile_academy=1)
+
+        self.bc.request.set_headers(academy=1)
+        self.bc.request.authenticate(model.user)
+
+        url = reverse_lazy('mentorship:academy_session_id', kwargs={'session_id': 1})
+        date = timezone.now()
+        data = [{
+            'name': self.bc.fake.name(),
+            'is_online': False,
+            'latitude': random.random() * 180 * random.choice([1, -1]),
+            'longitude': random.random() * 90 * random.choice([1, -1]),
+            'longitude': random.random() * 90 * random.choice([1, -1]),
+            'service': 1,
+            'mentee': 1,
+            'id': 1,
+            'online_meeting_url': self.bc.fake.url(),
+            'online_recording_url': self.bc.fake.url(),
+            'status': random.choice(statuses),
+            'allow_billing': bool(random.randint(0, 1)),
+            'bill': 1,
+            'agenda': self.bc.fake.text(),
+            'summary': self.bc.fake.text(),
+            'starts_at': self.bc.datetime.to_iso_string(date),
+            'ends_at': self.bc.datetime.to_iso_string(date),
+            'started_at': self.bc.datetime.to_iso_string(date),
+            'ended_at': self.bc.datetime.to_iso_string(date),
+            'mentor_joined_at': self.bc.datetime.to_iso_string(date),
+            'mentor_left_at': self.bc.datetime.to_iso_string(date),
+            'mentee_left_at': self.bc.datetime.to_iso_string(date),
+            'mentor': 1,
+        }]
+        response = self.client.put(url, data, format='json')
+
+        json = response.json()
+        expected = [put_serializer({
+            'id': 1,
+            'mentee': 1,
+            'service': 1,
+            **data[0],
+        })]
+
+        self.assertEqual(json, expected)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        update_fields = ['bill', 'mentee', 'mentor', 'service']
+        for key in update_fields:
+            data[0][f'{key}_id'] = data[0].pop(key)
+
+        self.assertEqual(self.bc.database.list_of('mentorship.MentorshipSession'), [
+            {
+                **self.bc.format.to_dict(model.mentorship_session),
+                **data[0],
+                'starts_at': date,
+                'ends_at': date,
+                'started_at': date,
+                'ended_at': date,
+                'mentor_joined_at': date,
+                'mentor_left_at': date,
+                'mentee_left_at': date,
+                'suggested_accounted_duration': timedelta(0),
+                'accounted_duration': timedelta(0),
+                'status_message': '',
+            },
+        ])
+        self.assertEqual(self.bc.database.list_of('mentorship.MentorshipBill'), [
+            self.bc.format.to_dict(model.mentorship_bill),
+        ])
+        self.assertEqual(
+            signals.mentorship_session_status.send.call_args_list,
+            ([
+                # mixer
+                call(instance=model.mentorship_session, sender=model.mentorship_session.__class__),
+            ] if current_status != 'PENDING' else []) + [
+                # endpoint
+                call(instance=model.mentorship_session, sender=model.mentorship_session.__class__),
+            ])
+
+    """
+    🔽🔽🔽 PUT with all required fields, is_online is False, MentorshipBill finished
+    """
+
+    @patch('breathecode.mentorship.signals.mentorship_session_status.send', MagicMock())
+    def test__put__found__with_all_required_fields__is_online_as_false__bill_finished(self):
+        mentorship_bill = {'status': random.choice(['APPROVED', 'PAID', 'IGNORED'])}
+        statuses = ['PENDING', 'STARTED', 'COMPLETED', 'FAILED', 'IGNORED']
+        current_status = random.choice(statuses)
+        statuses.remove(current_status)
+        mentorship_session = {'status': current_status}
+
+        model = self.bc.database.create(user=1,
+                                        role=1,
+                                        capability='crud_mentorship_session',
+                                        mentorship_session=mentorship_session,
+                                        mentorship_service=1,
+                                        mentorship_bill=mentorship_bill,
+                                        mentor_profile=1,
+                                        profile_academy=1)
+
+        self.bc.request.set_headers(academy=1)
+        self.bc.request.authenticate(model.user)
+
+        url = reverse_lazy('mentorship:academy_session_id', kwargs={'session_id': 1})
+        date = timezone.now()
+        data = [{
+            'name': self.bc.fake.name(),
+            'is_online': False,
+            'latitude': random.random() * 180 * random.choice([1, -1]),
+            'longitude': random.random() * 90 * random.choice([1, -1]),
+            'longitude': random.random() * 90 * random.choice([1, -1]),
+            'service': 1,
+            'mentee': 1,
+            'online_meeting_url': self.bc.fake.url(),
+            'online_recording_url': self.bc.fake.url(),
+            'status': random.choice(statuses),
+            'allow_billing': bool(random.randint(0, 1)),
+            'bill': 1,
+            'id': 1,
+            'agenda': self.bc.fake.text(),
+            'summary': self.bc.fake.text(),
+            'starts_at': self.bc.datetime.to_iso_string(date),
+            'ends_at': self.bc.datetime.to_iso_string(date),
+            'started_at': self.bc.datetime.to_iso_string(date),
+            'ended_at': self.bc.datetime.to_iso_string(date),
+            'mentor_joined_at': self.bc.datetime.to_iso_string(date),
+            'mentor_left_at': self.bc.datetime.to_iso_string(date),
+            'mentee_left_at': self.bc.datetime.to_iso_string(date),
+            'mentor': 1,
+        }]
+        response = self.client.put(url, data, format='json')
+
+        json = response.json()
+        expected = {'detail': 'trying-to-change-a-closed-bill', 'status_code': 400}
+
+        self.assertEqual(json, expected)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertEqual(self.bc.database.list_of('mentorship.MentorshipSession'), [
+            {
+                **self.bc.format.to_dict(model.mentorship_session),
+            },
+        ])
+        self.assertEqual(
+            str(signals.mentorship_session_status.send.call_args_list),
+            str([
+                call(instance=model.mentorship_session, sender=model.mentorship_session.__class__),
+            ] if current_status != 'PENDING' else []))
+        self.assertEqual(self.bc.database.list_of('mentorship.MentorshipBill'), [
+            self.bc.format.to_dict(model.mentorship_bill),
+        ])
+
+    """
+    🔽🔽🔽 PUT passing a MentorshipBill with some MentorshipSession without MentorshipService
+    """
+
+    @patch('breathecode.mentorship.signals.mentorship_session_status.send', MagicMock())
+    def test__put__found__passing_a_bill_with_some_session_without_service(self):
+        mentorship_bill = {'status': 'DUE'}
+        statuses = ['PENDING', 'STARTED', 'COMPLETED', 'FAILED', 'IGNORED']
+        current_status = random.choice(statuses)
+        statuses.remove(current_status)
+        mentorship_sessions = [{
+            'status': current_status,
+            'bill_id': 1,
+            'service_id': 1,
+        }, {
+            'status': current_status,
+            'bill_id': 1,
+            'service_id': None,
+        }]
+
+        model = self.bc.database.create(user=1,
+                                        role=1,
+                                        capability='crud_mentorship_session',
+                                        mentorship_session=mentorship_sessions,
+                                        mentorship_service=1,
+                                        mentorship_bill=mentorship_bill,
+                                        mentor_profile=1,
+                                        profile_academy=1)
+
+        self.bc.request.set_headers(academy=1)
+        self.bc.request.authenticate(model.user)
+
+        url = reverse_lazy('mentorship:academy_session_id', kwargs={'session_id': 1})
+        date = timezone.now()
+        data = [{
+            'name': self.bc.fake.name(),
+            'is_online': False,
+            'latitude': random.random() * 180 * random.choice([1, -1]),
+            'longitude': random.random() * 90 * random.choice([1, -1]),
+            'longitude': random.random() * 90 * random.choice([1, -1]),
+            'service': 1,
+            'mentee': 1,
+            'id': 1,
+            'online_meeting_url': self.bc.fake.url(),
+            'online_recording_url': self.bc.fake.url(),
+            'status': random.choice(statuses),
+            'allow_billing': bool(random.randint(0, 1)),
+            'bill': 1,
+            'agenda': self.bc.fake.text(),
+            'summary': self.bc.fake.text(),
+            'starts_at': self.bc.datetime.to_iso_string(date),
+            'ends_at': self.bc.datetime.to_iso_string(date),
+            'started_at': self.bc.datetime.to_iso_string(date),
+            'ended_at': self.bc.datetime.to_iso_string(date),
+            'mentor_joined_at': self.bc.datetime.to_iso_string(date),
+            'mentor_left_at': self.bc.datetime.to_iso_string(date),
+            'mentee_left_at': self.bc.datetime.to_iso_string(date),
+            'mentor': 1,
+        }]
+        response = self.client.put(url, data, format='json')
+
+        json = response.json()
+        expected = [put_serializer({
+            'id': 1,
+            'mentee': 1,
+            'service': 1,
+            **data[0],
+        })]
+
+        self.assertEqual(json, expected)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        update_fields = ['bill', 'mentee', 'mentor', 'service']
+        for key in update_fields:
+            data[0][f'{key}_id'] = data[0].pop(key)
+
+        self.assertEqual(self.bc.database.list_of('mentorship.MentorshipSession'), [
+            {
+                **self.bc.format.to_dict(model.mentorship_session[0]),
+                **data[0],
+                'starts_at': date,
+                'ends_at': date,
+                'started_at': date,
+                'ended_at': date,
+                'mentor_joined_at': date,
+                'mentor_left_at': date,
+                'mentee_left_at': date,
+            },
+            self.bc.format.to_dict(model.mentorship_session[1]),
+        ])
+        self.assertEqual(self.bc.database.list_of('mentorship.MentorshipBill'), [
+            {
+                **self.bc.format.to_dict(model.mentorship_bill),
+                'status': 'RECALCULATE',
+            },
+        ])
+        self.assertEqual(
+            signals.mentorship_session_status.send.call_args_list,
+            ([
+                # mixer
+                call(instance=model.mentorship_session[0], sender=model.mentorship_session[0].__class__),
+                call(instance=model.mentorship_session[1], sender=model.mentorship_session[1].__class__),
+            ] if current_status != 'PENDING' else []) + [
+                # endpoint
+                call(instance=model.mentorship_session[0], sender=model.mentorship_session[0].__class__),
+            ])
