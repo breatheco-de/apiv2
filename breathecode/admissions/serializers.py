3f11feede011968fc566e721f1a3e61be81125b8
Alejandro Sanchez <aalejo@gmail.com>
2022-09-09 23:19:52 -0400

    Update serializers.py

--- breathecode/admissions/serializers.py
+++ breathecode/admissions/serializers.py
@@ -900,7 +900,7 @@ class SyllabusSerializer(serializers.ModelSerializer):
 
 class SyllabusVersionSerializer(serializers.ModelSerializer):
     json = serializers.JSONField()
-
+    
     class Meta:
         model = SyllabusVersion
         fields = ['json', 'version', 'syllabus', 'status', 'change_log_details']
@@ -931,11 +931,12 @@ class SyllabusVersionSerializer(serializers.ModelSerializer):
 
 
 class SyllabusVersionPutSerializer(serializers.ModelSerializer):
-    json = serializers.JSONField()
-
+    json = serializers.JSONField(required=False)
+    status = serializers.CharField(required=False)
+    
     class Meta:
         model = SyllabusVersion
-        fields = ['json', 'version', 'syllabus']
+        fields = ['json', 'version', 'syllabus', 'status']
         exclude = ()
         extra_kwargs = {
             'syllabus': {
