c6abf2309b0c0aaa5e85bc42068374b049179745
Alejandro Sanchez <aalejo@gmail.com>
2022-09-22 23:46:56 -0400

    Update actions.py

--- breathecode/authenticate/actions.py
+++ breathecode/authenticate/actions.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from .models import DeviceId, Token, Role, ProfileAcademy, GitpodUser, CredentialsGithub
 from breathecode.notify.actions import send_email_message
+from breathecode.utils.views import render_message
 from breathecode.admissions.models import Academy
 from random import randint
 
