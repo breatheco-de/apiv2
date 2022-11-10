535d0be9154853c1d40451129d243b5954386424
Alejandro Sanchez <aalejo@gmail.com>
2022-11-10 04:26:59 +0000

    trying to fix new attendancy log

--- breathecode/admissions/utils/cohort_log.py
+++ breathecode/admissions/utils/cohort_log.py
@@ -1,4 +1,6 @@
 import logging
+from django.utils import timezone
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -13,12 +15,19 @@ def has_duplicates(values):
 class CohortDayLog(object):
     current_module = None
     teacher_comments = None
+    created_at = None
     attendance_ids: []
     unattendance_ids: []
 
-    def __init__(self, current_module: str, teacher_comments: str, attendance_ids: list, unattendance_ids: list):
+    def __init__(self,
+                 current_module: str = None,
+                 teacher_comments: str = None,
+                 attendance_ids: list = None,
+                 unattendance_ids: list = None,
+                 updated_at: datetime = None,
+                 allow_empty=False):
 
-        if not isinstance(current_module, str):
+        if not isinstance(current_module, str) and (not allow_empty and current_module is None):
             raise Exception(f'Invalid current module value {str(current_module)}')
         if teacher_comments is not None and not isinstance(teacher_comments, str):
             raise Exception(f'Invalid teacher comments value {str(teacher_comments)}')
@@ -26,10 +35,12 @@ class CohortDayLog(object):
             raise Exception(f'Invalid attendance list, it must be an array of integer ids')
         if not isinstance(unattendance_ids, list):
             raise Exception(f'Invalid unattendance list, it must be an array of integer ids')
+        if updated_at is None:
+            updated_at = timezone.now()
 
         if has_duplicates(attendance_ids):
             raise Exception(f'Attendance list has duplicate user ids')
-            
+
         if has_duplicates(unattendance_ids):
             raise Exception(f'Unattendance list has duplicate user ids')
 
@@ -37,15 +48,19 @@ class CohortDayLog(object):
         self.teacher_comments = teacher_comments
         self.attendance_ids = attendance_ids
         self.unattendance_ids = unattendance_ids
-    
+        self.updated_at = updated_at
+
     @staticmethod
     def empty():
-        return {
-            'current_module': None,
-            'teacher_comments': None,
-            'attendance_ids': [],
-            'unattendance_ids': []
-        }
+        return CohortDayLog(
+            **{
+                'current_module': None,
+                'teacher_comments': None,
+                'updated_at': None,
+                'attendance_ids': [],
+                'unattendance_ids': [],
+                'allow_empty': True
+            })
 
     def serialize(self):
         return {
@@ -53,52 +68,67 @@ class CohortDayLog(object):
             'teacher_comments': self.teacher_comments,
             'attendance_ids': self.attendance_ids,
             'unattendance_ids': self.unattendance_ids,
+            'updated_at': str(self.updated_at),
         }
 
 
 class CohortLog(object):
-    cohort = None
     days = []
+    cohort = None
 
     def __init__(self, cohort):
+
+        self.days = []
+        self.cohort = None
 
         if cohort is None:
             raise Exception("Cohort log cannot be retrived because it's null")
 
-        if cohort.history_log is None:
-            cohort.history_log = [CohortDayLog.empty() for i in range(0,self.cohort.current_day)]
-            
-        elif not isinstance(cohort.history_log, list):
-            raise Exception('Cohort history json must be in list format')
-            
-        if len(cohort.history_log) != self.cohort.current_day:
-            raise Exception(f'Cohort log must have exactly {self.cohort.current_day} days but it has {len(cohort.history_log)}')
-
         self.cohort = cohort
         if self.cohort.current_day == 0:
             self.cohort.current_day = 1
-        self.days = [CohortDayLog(**d) for d in self.cohort.history_log]
+
+        if self.cohort.history_log is None:
+            self.cohort.history_log = {}
+
+        elif not isinstance(self.cohort.history_log, dict):
+            raise Exception('Cohort history json must be in dictionary format')
+
+        for day in range(1, self.cohort.current_day + 1):
+            if str(day) in self.cohort.history_log:
+                self.days = [*self.days, CohortDayLog(**self.cohort.history_log[str(day)], allow_empty=True)]
+            else:
+                self.days = [*self.days, CohortDayLog.empty()]
 
     def logDay(self, payload, day=None):
 
         if not isinstance(payload, dict):
             raise Exception('Entry log of cohort day must be a dictionary')
-            
+
         if day is None:
             day = self.cohort.current_day
-            
+
+        if day == 0:
+            raise Exception('Invalid log for day index=0, cohort days start at 1')
+
         elif day > self.cohort.current_day:
-            raise Exception(f'You cannot log activity for day {str(day)} because the cohort is currently at day {str(self.cohort.current_day)}')
+            raise Exception(
+                f'You cannot log activity for day {str(day)} because the cohort is currently at day {str(self.cohort.current_day)}'
+            )
 
         try:
             self.days[day - 1] = CohortDayLog(**payload)
-            logger.debug(f'Updated cohort {self.cohort.slug} log for day {day}')
+            logger.debug(f'Replaced cohort {self.cohort.slug} log for day {day}')
         except IndexError as e:
-            logger.debug(f'Replacing cohort {self.cohort.slug} log for day {day}')
-            self.days.append(CohortDayLog(**payload))
+            raise Exception(f'Error adding day {str(day-1)} log to cohort')
 
     def save(self):
-        self.cohort.history_log = [day.serialize() for day in self.days]
+
+        count = 0
+        for day in self.days:
+            count += 1
+            self.cohort.history_log[str(count)] = day.serialize()
+
         self.cohort.save()
 
     def serialize(self):
