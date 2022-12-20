b2278c9795ad0c41fe1fb9dd1ef830ca1a186675
Andresgarciasch <andres_28garcia@outlook.es>
2022-12-20 22:11:23 +0000

    revision de command

--- breathecode/registry/management/commands/change_asset_readme_url.py
+++ breathecode/registry/management/commands/change_asset_readme_url.py
@@ -5,15 +5,22 @@ from ...models import Asset
 from breathecode.admissions.models import Academy
 from ...tasks import async_pull_from_github
 from slugify import slugify
+import re
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = 'This command changes every readme_url instance that look like this >>https://raw.githubusercontent.com/breatheco-de/exercise-postcard/main/README.md and turned into one that looks like this >>https://github.com/breatheco-de/exercise-postcard/blob/main/README.md'
+    help = 'This command changes every readme_url instance with this structure >>https://raw.githubusercontent.com/breatheco-de/exercise-postcard/main/README.md and turns it in one with this structure >>https://github.com/breatheco-de/exercise-postcard/blob/main/README.md'
 
     def handle(self, *args, **options):
 
-        readme = Asset.objects.filter(
-            readme_url='https://raw.githubusercontent.com/breatheco-de/exercise-postcard/main/README.md'
-        ).update(readme_url='https://github.com/breatheco-de/exercise-postcard/blob/main/README.md')
+        assets = Asset.objects.all()
+        for asset in assets:
+            readme_urls = asset.readme_url
+            regex = re.search('^https://raw.githubusercontent.com', readme_urls)
+            if regex:
+                link_split = readme_urls.split('/')
+                new_link = 'https://github.com' + '/' + link_split[3] + '/' + link_split[
+                    4] + '/' + 'blob/main/README.md'
+                readme_urls = new_link
--- breathecode/registry/tests/management/commands/test_change_asset_readme_url.py
+++ breathecode/registry/tests/management/commands/test_change_asset_readme_url.py
@@ -14,8 +14,10 @@ class ChangeAssetReadmeUrlTestCase(RegistryTestCase):
     '''
 
     @patch('django.core.management.base.OutputWrapper.write', MagicMock())
-    def test_change_asset_readme_url(self):
+    def test_change_asset_readme_url_without_readme(self):
         from django.core.management.base import OutputWrapper
+
+        # model = self.bc.database.create(readme_url='https://raw.githubusercontent.com/breatheco-de/exercise-postcard/main/README.md')
 
         command = Command()
 
@@ -23,8 +25,50 @@ class ChangeAssetReadmeUrlTestCase(RegistryTestCase):
 
         self.assertEqual(result, None)
 
-        self.assertEqual(OutputWrapper.write.call_args_list, [])
-
         self.assertEqual(OutputWrapper.write.call_count, 0)
 
         self.assertEqual(self.bc.database.list_of('registry.Asset'), [])
+
+        self.assertEqual(OutputWrapper.write.call_args_list, [])
+
+    # @patch('django.core.management.base.OutputWrapper.write', MagicMock())
+    # def test_change_asset_readme_url(self):
+    #     """
+    #     Descriptions of models are being generated:
+
+    #       Academy(id=1):
+    #         city: City(id=1)
+    #         country: Country(code="UgN")
+
+    #       Cohort(id=1):
+    #         academy: Academy(id=1)
+    #         schedule: SyllabusSchedule(id=1)
+
+    #       SyllabusSchedule(id=1):
+    #         academy: Academy(id=1)
+    #     """
+    #     from django.core.management.base import OutputWrapper
+
+    #     readme = [{
+    #         'readme_url':
+    #         'https://raw.githubusercontent.com/breatheco-de/exercise-postcard/main/README.md'
+    #     }]
+
+    #     model = self.bc.database.create(asset=readme)
+
+    #     command = Command()
+
+    #     result = command.handle()
+
+    #     self.assertEqual(result, None)
+
+    #     #self.assertEqual(self.bc.database.list_of('registry.Asset'), [])
+
+    #     self.assertEqual(self.bc.database.list_of('registry.Asset'), [
+    #         {
+    #             **self.bc.format.to_dict(model.asset),
+    #             'readme_url':
+    #             'https://github.com/breatheco-de/exercise-postcard/blob/main/README.md',
+    #         },
+    #     ])
+    #     self.assertEqual(OutputWrapper.write.call_args_list, [call('Done!')])
