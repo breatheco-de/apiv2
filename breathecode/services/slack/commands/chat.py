10a703b3a15d4d58deb58b69ae2800a4ef404568
Alejandro Sanchez <aalejo@gmail.com>
2022-12-26 22:13:43 +0000

    openai

---
+++ breathecode/services/slack/commands/chat.py
@@ -0,0 +1,49 @@
+"""
+Possible parameters for this command:
+Ask my anything
+
+"""
+import os
+
+from breathecode.admissions.models import Cohort, CohortUser
+from ..decorator import command
+from ..utils import to_string
+from ..exceptions import SlackException
+
+
+@command(capable_of='read_cohort')
+def execute(channel_id, academies, **context):
+
+    response = {'blocks': []}
+    response['blocks'].append(render_cohort(channel_id=channel_id, academies=academies))
+
+    return response
+
+
+def render_cohort(channel_id, academies):
+
+    cohort = Cohort.objects.filter(slackchannel__slack_id=channel_id, academy__id__in=[academies]).first()
+    if cohort is None:
+        raise SlackException(
+            f'Cohort was not found as slack channel, make sure the channel name matches the cohort slug',
+            slug='cohort-not-found')
+
+    teachers = CohortUser.objects.filter(cohort=cohort,
+                                         role__in=['TEACHER', 'ASSISTANT'],
+                                         cohort__academy__id__in=[academies])
+    return {
+        'type': 'section',
+        'text': {
+            'type':
+            'mrkdwn',
+            'text':
+            f"""
+*Cohort name:* {cohort.name}
+*Start Date:* {cohort.kickoff_date}
+*End Date:* {cohort.ending_date}
+*Current day:* {cohort.current_day}
+*Stage:* {cohort.stage}
+*Teachers:* {', '.join([cu.user.first_name + ' ' + cu.user.last_name for cu in teachers])}
+"""
+        }
+    }
