6fa96f26b1be19326e603aca3f88cf84ad22055a
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2023-01-06 23:05:24 +0000

    academy_default functionality removed from is_hidden_on_prework

--- breathecode/admissions/views.py
+++ breathecode/admissions/views.py
@@ -1177,9 +1177,6 @@ class AcademyCohortView(APIView, GenerateLookupsMixin):
         for key in request.data:
             data[key] = request.data.get(key)
 
-        if 'is_hidden_on_prework' not in data or data['is_hidden_on_prework'] == 'academy_default':
-            data['is_hidden_on_prework'] = academy.is_hidden_on_prework
-
         if 'timezone' not in data:
             data['timezone'] = academy.timezone
 
@@ -1219,9 +1216,6 @@ class AcademyCohortView(APIView, GenerateLookupsMixin):
 
         if 'syllabus_version' in data:
             del data['syllabus_version']
-
-        if 'is_hidden_on_prework' in data and data['is_hidden_on_prework'] == 'academy_default':
-            data['is_hidden_on_prework'] = academy.is_hidden_on_prework
 
         serializer = CohortPUTSerializer(cohort,
                                          data=data,
