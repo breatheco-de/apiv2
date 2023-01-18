9e2e32ed1762b042b5eb890c21a6f59da9fa8963
jefer94 <jdefreitaspinto@gmail.com>
2023-01-17 19:30:17 -0500

    fix live class on save

--- breathecode/events/models.py
+++ breathecode/events/models.py
@@ -304,7 +304,7 @@ class LiveClass(models.Model):
     updated_at = models.DateTimeField(auto_now=True, editable=False)
 
     def save(self, *args, **kwargs):
-        if not self.key:
+        if not self.hash:
             self.hash = binascii.hexlify(os.urandom(20)).decode()
 
         return super().save(*args, **kwargs)
