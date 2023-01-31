751bf8fd5d1b73824bc80f6baf206b14c560bb83
Andresgarciasch <andres_28garcia@outlook.es>
2023-01-31 17:15:15 +0000

    visibility validation

--- breathecode/registry/serializers.py
+++ breathecode/registry/serializers.py
@@ -573,6 +573,16 @@ class AssetPUTSerializer(serializers.ModelSerializer):
                 raise ValidationException(f'This asset has to pass tests successfully before publishing',
                                           status.HTTP_400_BAD_REQUEST)
 
+        if 'visibility' in data and data['visibility'] == 'PUBLIC':
+            if self.instance.test_visibility != 'OK':
+                raise ValidationException(f'This asset has to pass tests successfully before publishing',
+                                          status.HTTP_400_BAD_REQUEST)
+
+        if 'visibility' in data and data['visibility'] == 'UNLISTED':
+            if self.instance.test_visibility != 'OK':
+                raise ValidationException(f'This asset has to pass tests successfully before publishing',
+                                          status.HTTP_400_BAD_REQUEST)
+
         if 'slug' in data:
             data['slug'] = slugify(data['slug']).lower()
 
