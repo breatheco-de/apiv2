f4851ee4122be208fa84c4feb07706f9a39f4412
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2023-02-06 13:21:59 -0400

    Update client.py

--- breathecode/services/seo/client.py
+++ breathecode/services/seo/client.py
@@ -14,9 +14,9 @@ class SEOAnalyzer:
     shared_state = {}
     influence = {
         'general_structure': 0.2,
-        'keyword_density': 0.325,
-        'internal_linking': 0.325,
-        'images_use': 0.15
+        'keyword_density': 0.375,
+        'internal_linking': 0.375,
+        'images_use': 0.05
     }
 
     def __init__(self, asset, exclude=[]):
