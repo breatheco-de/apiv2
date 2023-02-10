f73cdcb9202cd567212a977e239a238c91cc550d
gustavomm19 <gustavomm199@gmail.com>
2023-02-10 20:11:13 +0000

    add tests

--- breathecode/marketing/tests/management/commands/tests_retry_pending_leads.py
+++ breathecode/marketing/tests/management/commands/tests_retry_pending_leads.py
@@ -9,21 +9,49 @@ from breathecode.marketing.tests.mixins import MarketingTestCase
 UTC_NOW = timezone.now()
 
 
+def serialize_form_entry(form_entry):
+    return {
+        'id': form_entry['id'],
+        'first_name': form_entry['first_name'],
+        'last_name': form_entry['last_name'],
+        'phone': form_entry['phone'],
+        'email': form_entry['email'],
+        'location': form_entry['location'],
+        'referral_key': form_entry['referral_key'],
+        'course': form_entry['course'],
+        'tags': form_entry['tags'],
+        'automations': form_entry['automations'],
+        'language': form_entry['language'],
+        'city': form_entry['city'],
+        'country': form_entry['country'],
+        'utm_url': form_entry['utm_url'],
+        'client_comments': form_entry['client_comments'],
+        'current_download': form_entry['current_download'],
+        'latitude': form_entry['latitude'],
+        'longitude': form_entry['longitude']
+    }
+
+
 class RetryPendingLeadsTestSuite(MarketingTestCase):
     """
     🔽🔽🔽 With no form entries
     """
 
+    @patch('breathecode.marketing.tasks.persist_single_lead.delay', MagicMock())
+    @patch('django.utils.timezone.now', MagicMock(return_value=UTC_NOW))
     def test_without_formentries(self):
         command = Command()
         command.handle()
 
         self.assertEqual(self.bc.database.list_of('marketing.FormEntry'), [])
+        self.assertEqual(tasks.persist_single_lead.delay.call_args_list, [])
 
     """
     🔽🔽🔽 With form entries not pending
     """
 
+    @patch('breathecode.marketing.tasks.persist_single_lead.delay', MagicMock())
+    @patch('django.utils.timezone.now', MagicMock(return_value=UTC_NOW))
     def test_without_pending_formentries(self):
         model = self.bc.database.create(
             form_entry=2,
@@ -34,23 +62,25 @@ class RetryPendingLeadsTestSuite(MarketingTestCase):
 
         self.assertEqual(self.bc.database.list_of('marketing.FormEntry'),
                          self.bc.format.to_dict(model.form_entry))
+        self.assertEqual(tasks.persist_single_lead.delay.call_args_list, [])
 
     """
     🔽🔽🔽 With form entries pending
     """
 
+    @patch('breathecode.marketing.tasks.persist_single_lead.delay', MagicMock())
+    @patch('django.utils.timezone.now', MagicMock(return_value=UTC_NOW))
     def test_with_pending_formentries(self):
         model = self.bc.database.create(
-            form_entry=2,
+            form_entry=True,
             form_entry_kwargs={'storage_status': 'PENDING'},
         )
         command = Command()
         result = command.handle()
 
-        print('result')
-        for item in self.bc.format.to_dict(model.form_entry):
-            print(item['id'])
-            print(item['storage_status'])
+        model_dict = self.bc.format.to_dict(model.form_entry)
 
         self.assertEqual(self.bc.database.list_of('marketing.FormEntry'),
-                         self.bc.format.to_dict(model.form_entry))
+                         self.bc.format.to_dict([model.form_entry]))
+        self.assertEqual(tasks.persist_single_lead.delay.call_args_list,
+                         [call(serialize_form_entry(model_dict))])
