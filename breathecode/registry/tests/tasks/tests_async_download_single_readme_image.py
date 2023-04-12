a6b1e20598ab05dac8ee52a0009df60c1df9a99d
gustavomm19 <gustavomm199@gmail.com>
2023-04-12 18:00:24 +0000

    add encode

--- breathecode/registry/tests/tasks/tests_async_download_single_readme_image.py
+++ breathecode/registry/tests/tasks/tests_async_download_single_readme_image.py
@@ -2,6 +2,7 @@
 Test async_download_single_readme_image
 """
 from random import randint
+import base64
 from unittest.mock import MagicMock, call, patch, PropertyMock
 
 from breathecode.registry.tasks import async_download_single_readme_image
@@ -127,11 +128,17 @@ class RegistryTestSuite(RegistryTestCase):
            })))
     def test__with_download_status_not_ok(self):
         asset_image = {'name': 'john', 'original_url': original_url, 'bucket_url': 'https://www.f.com'}
-        model = self.bc.database.create(asset={'slug': 'fake_slug'}, asset_image=asset_image)
+        fake_readme = 'hi https://www.f.com'
+        encoded_readme = base64.b64encode(fake_readme.encode('utf-8')).decode('utf-8')
 
-        readme = model['asset'].get_readme()
-        model['asset'].set_readme(readme['decoded'] + ' https://www.f.com')
-        model['asset'].save()
+        model = self.bc.database.create(asset={
+            'slug': 'fake_slug',
+            'readme': encoded_readme,
+            'readme_raw': encoded_readme
+        },
+                                        asset_image=asset_image)
+        #store the original readme_raw to verify it does not get modified
+        readme_raw = model['asset'].readme_raw
 
         result = async_download_single_readme_image('fake_slug', 'https://www.f.com')
         #The content is static in the decorator, so the hash is always the same
@@ -141,6 +148,64 @@ class RegistryTestSuite(RegistryTestCase):
         asset_image = self.bc.database.get_model('registry.AssetImage').objects.first()
         self.assertEqual(result, 'OK')
         self.assertEqual('https://xyz/hardcoded_url' in readme, True)
+        self.assertEqual(readme_raw, self.bc.database.get_model('registry.asset').objects.first().readme_raw)
+        self.assertEqual(self.bc.database.list_of('registry.AssetImage'), [{
+            'id': 1,
+            'bucket_url': 'https://xyz/hardcoded_url',
+            'original_url': original_url,
+            'download_details': 'Downloading https://www.f.com',
+            'download_status': 'OK',
+            'hash': hash,
+            'last_download_at': None,
+            'mime': 'image/png',
+            'name': 'john',
+        }])
+
+    @patch('requests.get',
+           apply_requests_get_mock([(200, original_url, {
+               'headers': {
+                   'content-type': 'image/png'
+               }
+           }, {
+               'content-type': 'image/png'
+           })]))
+    @patch.multiple('breathecode.services.google_cloud.Storage',
+                    __init__=MagicMock(return_value=None),
+                    client=PropertyMock(),
+                    create=True)
+    @patch.multiple('breathecode.services.google_cloud.File',
+                    __init__=MagicMock(return_value=None),
+                    bucket=PropertyMock(),
+                    file_name=PropertyMock(),
+                    blob=PropertyMock(side_effect=[None, 1]),
+                    upload=MagicMock(),
+                    url=MagicMock(return_value='https://xyz/hardcoded_url'),
+                    create=True)
+    @patch('os.getenv',
+           MagicMock(side_effect=apply_get_env({
+               'GOOGLE_PROJECT_ID': 'labor-day-story',
+               'MEDIA_GALLERY_BUCKET': 'bucket-name',
+           })))
+    def test__with_download_status_not_ok_many_images(self):
+        asset_image = {'name': 'john', 'original_url': original_url, 'bucket_url': 'https://www.f.com'}
+        fake_readme = 'hi https://www.f.com https://www.f.com https://www.f.com'
+        encoded_readme = base64.b64encode(fake_readme.encode('utf-8')).decode('utf-8')
+
+        model = self.bc.database.create(asset={
+            'slug': 'fake_slug',
+            'readme': encoded_readme
+        },
+                                        asset_image=asset_image)
+
+        result = async_download_single_readme_image('fake_slug', 'https://www.f.com')
+        #The content is static in the decorator, so the hash is always the same
+        hash = '5186bd77843e507d2c6f568d282c56b06622b2fc7d6ae6a109c97ee1fc3cdebc'
+
+        readme = self.bc.database.get_model('registry.asset').objects.first().get_readme()['decoded']
+        asset_image = self.bc.database.get_model('registry.AssetImage').objects.first()
+        self.assertEqual(result, 'OK')
+        self.assertEqual('https://xyz/hardcoded_url' in readme, True)
+        self.assertEqual('https://www.f.com' not in readme, True)
         self.assertEqual(self.bc.database.list_of('registry.AssetImage'), [{
             'id': 1,
             'bucket_url': 'https://xyz/hardcoded_url',
@@ -164,11 +229,14 @@ class RegistryTestSuite(RegistryTestCase):
            })))
     def test__with_ok_download_status(self):
         asset_image = {'name': 'john', 'bucket_url': 'https://www.f.com', 'download_status': 'OK'}
-        model = self.bc.database.create(asset={'slug': 'fake_slug'}, asset_image=asset_image)
+        fake_readme = 'hi https://www.f.com'
+        encoded_readme = base64.b64encode(fake_readme.encode('utf-8')).decode('utf-8')
 
-        readme = model['asset'].get_readme()
-        model['asset'].set_readme(readme['decoded'] + ' https://www.f.com')
-        model['asset'].save()
+        model = self.bc.database.create(asset={
+            'slug': 'fake_slug',
+            'readme': encoded_readme
+        },
+                                        asset_image=asset_image)
 
         result = async_download_single_readme_image('fake_slug', 'https://www.f.com')
 
