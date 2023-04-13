f5cfed4e7b57c4413e44a7a16e354c19f9a1947a
jefer94 <jdefreitaspinto@gmail.com>
2023-04-13 17:05:17 -0500

    add fix timeslots

---
+++ breathecode/events/management/commands/fix_live_classes.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+
+from breathecode.admissions.models import Cohort, CohortTimeSlot
+from django.utils import timezone
+
+from breathecode.events import tasks
+
+
+class Command(BaseCommand):
+    help = 'Build live classes'
+
+    def handle(self, *args, **options):
+        utc_now = timezone.now()
+
+        cohorts = Cohort.objects.filter(ending_date__gte=utc_now,
+                                        never_ends=False).exclude(stage__in=['DELETED', 'PREWORK'])
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f'Found {str(cohorts.count())} cohorts that have not finished and should have live classes'))
+
+        for cohort in cohorts:
+            timeslots = CohortTimeSlot.objects.filter(cohort=cohort)
+            total_cohort_timeslots = timeslots.count()
+            if total_cohort_timeslots == 0:
+                continue
+            else:
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f'Adding cohort {cohort.slug} to the fixing queue, it ends on {str(cohort.ending_date)}'
+                    ))
+                for timeslot in timeslots:
+                    tasks.fix_live_classes_from_timeslot.delay(timeslot.id)
--- breathecode/events/tasks.py
+++ breathecode/events/tasks.py
@@ -148,3 +148,76 @@ def build_live_classes_from_timeslot(self, timeslot_id: int):
         ending_at += delta
 
     live_classes.delete()
+
+
+@shared_task(bind=True, base=BaseTaskWithRetry)
+def fix_live_classes_from_timeslot(self, timeslot_id: int):
+    logger.info(f'Starting fix_live_classes_from_timeslot with id {timeslot_id}')
+
+    timeslot = CohortTimeSlot.objects.filter(id=timeslot_id).first()
+    if not timeslot:
+        logger.error(f'Timeslot {timeslot_id} not fount')
+        return
+
+    utc_now = timezone.now()
+
+    cohort = timeslot.cohort
+    # live_classes = LiveClass.objects.filter(cohort_time_slot=timeslot, starting_at__gte=utc_now)
+
+    starting_at = DatetimeInteger.to_datetime(timeslot.timezone, timeslot.starting_at)
+    ending_at = DatetimeInteger.to_datetime(timeslot.timezone, timeslot.ending_at)
+    until_date = timeslot.removed_at or cohort.ending_date
+    start_date = cohort.kickoff_date
+
+    # this event end in the new day
+    if starting_at > ending_at:
+        ending_at.year = starting_at.year
+        ending_at.month = starting_at.month
+        ending_at.day = starting_at.day + 1
+
+    if not until_date:
+        logger.error(f'Timeslot {timeslot_id} not have a ending date')
+        # live_classes.delete()
+
+        return
+
+    delta = relativedelta(0)
+
+    if timeslot.recurrency_type == 'DAILY':
+        delta += relativedelta(days=1)
+
+    if timeslot.recurrency_type == 'WEEKLY':
+        delta += relativedelta(weeks=1)
+
+    if timeslot.recurrency_type == 'MONTHLY':
+        delta += relativedelta(months=1)
+
+    if not delta:
+        logger.error(f'{timeslot.recurrency_type} is not a valid or not implemented recurrency_type')
+        return
+
+    while True:
+
+        if ending_at > until_date:
+            break
+
+        if starting_at > start_date:
+            try:
+                schedule, _ = LiveClass.objects.get(starting_at=starting_at,
+                                                    ending_at=ending_at,
+                                                    cohort_time_slot=timeslot)
+            except LiveClass.DoesNotExist:
+                schedule = LiveClass.objects.create(starting_at=starting_at,
+                                                    ending_at=ending_at,
+                                                    cohort_time_slot=timeslot,
+                                                    remote_meeting_url=cohort.online_meeting_url or '')
+
+            # live_classes = live_classes.exclude(id=schedule.id)
+
+            if not timeslot.recurrent:
+                break
+
+        starting_at += delta
+        ending_at += delta
+
+    # live_classes.delete()
