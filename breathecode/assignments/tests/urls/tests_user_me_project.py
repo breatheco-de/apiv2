a502db0e44e82e626c34819fdbc45d7cb65454ed
gustavomm19 <gustavomm199@gmail.com>
2023-04-24 18:15:08 +0000

    add status 200 tets

--- breathecode/assignments/tests/urls/tests_user_me_project.py
+++ breathecode/assignments/tests/urls/tests_user_me_project.py
@@ -11,6 +11,22 @@ from rest_framework import status
 from ..mixins import AssignmentsTestCase
 
 
+def put_serializer(self, project, data={}):
+    return {
+        'id': project.id,
+        'logo_url': project.logo_url,
+        'name': project.name,
+        'one_line_desc': project.one_line_desc,
+        'public_url': project.public_url,
+        'cohort': project.cohort.id if project.cohort else None,
+        'created_at': self.bc.datetime.to_iso_string(project.created_at),
+        'description': project.description,
+        'revision_status': project.revision_status,
+        'revision_message': project.revision_message,
+        **data,
+    }
+
+
 class FinalProjectTestSuite(AssignmentsTestCase):
     """Test /final_project"""
     """
@@ -105,3 +121,37 @@ class FinalProjectTestSuite(AssignmentsTestCase):
 
         self.assertEqual(json, expected)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_final_project(self):
+
+        self.bc.request.set_headers(academy=1)
+        helper_models = self.bc.database.create(user=1, cohort=1)
+        cohort_user = self.bc.database.create(cohort_user={
+            'user': helper_models['user'],
+            'cohort': helper_models['cohort']
+        })
+
+        project_cohort = helper_models['cohort']
+
+        models = self.bc.database.create(final_project={
+            'members': [helper_models['user']],
+            'cohort': project_cohort
+        })
+
+        self.bc.request.authenticate(helper_models['user'])
+        url = reverse_lazy('assignments:user_me_project', kwargs={'project_id': 1})
+        data = {'name': 'Facebook', 'cohort': project_cohort.id, 'members': [1]}
+        response = self.client.put(url, data)
+
+        json = response.json()
+        expected = {'detail': 'not-a-member', 'status_code': 400}
+        print(json, 'json')
+
+        self.assertEqual(
+            json,
+            put_serializer(self, models['final_project'], {
+                'name': 'Facebook',
+                'cohort': project_cohort.id,
+                **json
+            }))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
