eeea90bcfe4dfb4a609428904987e1d290f183d1
Alejandro Sanchez <aalejo@gmail.com>
2023-07-19 23:19:18 +0000

    we are now keeping in sync the githubacademyuser username with the credentialgithub username

--- breathecode/authenticate/management/commands/sync_github_usernames.py
+++
@@ -1,8 +0,0 @@
-from breathecode.authenticate.models import GithubAcademyUser, CredentialsGithub
-from django.contrib.auth.models import User
-
-missing_usernames = GithubAcademyUser.objects.filter(username__isnull=True)
-for github_user in missing_usernames:
-    credential = CredentialsGithub.objects.filter(user=github_user.user).first()
-    if credential is not None:
-        GithubAcademyUser.objects.filter(user=credential.user).update(username=credential.username)
