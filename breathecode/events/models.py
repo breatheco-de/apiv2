29c4b1cde425df2d4e1d4c8fadc83918faccb9d1
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2023-07-26 19:33:13 +0000

    Events slug made unique

--- breathecode/events/models.py
+++ breathecode/events/models.py
@@ -176,7 +176,7 @@ class Event(models.Model):
         super(Event, self).__init__(*args, **kwargs)
         self.__old_status = self.status
 
-    slug = models.SlugField(max_length=150, blank=True, default=None, null=True)
+    slug = models.SlugField(max_length=150, blank=True, default=None, null=True, unique=True)
     description = models.TextField(max_length=2000, blank=True, default=None, null=True)
     excerpt = models.TextField(max_length=500, blank=True, default=None, null=True)
     title = models.CharField(max_length=255, blank=True, default=None, null=True)
