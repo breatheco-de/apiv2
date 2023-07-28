94baecc4778d4f73e082bff9e585b58212cfbf92
jefer94 <jdefreitaspinto@gmail.com>
2023-07-28 12:35:34 -0500

    check dependencies

---
+++ scripts/md5.py
@@ -0,0 +1,19 @@
+#!/bin/env python
+
+from __future__ import absolute_import
+import hashlib
+import os
+import sys
+
+if __name__ == '__main__':
+    args = ''
+
+    if len(sys.argv) <= 1:
+        Exception('No arguments passed')
+
+    with open(sys.argv[1], 'rb') as f:
+        content = f.read()
+
+    hash = hashlib.md5(content).hexdigest()
+    print(hash)
+1
