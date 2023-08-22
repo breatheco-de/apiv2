ee95ca66fa4a8bdf4263b27813aefe362195ec67
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2023-08-22 13:55:23 +0000

    Event.uuid created default=None

--- breathecode/events/models.py
+++ breathecode/events/models.py
@@ -173,7 +173,7 @@ class Event(models.Model):
         self.__old_status = self.status
 
     slug = models.SlugField(max_length=150, blank=True, default=None, null=True)
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    uuid = models.UUIDField(blank=True, null=True, default=None, editable=False, unique=False)
     description = models.TextField(max_length=2000, blank=True, default=None, null=True)
     excerpt = models.TextField(max_length=500, blank=True, default=None, null=True)
     title = models.CharField(max_length=255, blank=True, default=None, null=True)
@@ -270,6 +270,13 @@ class Event(models.Model):
         created = not self.id
 
         if created:
+            while True:
+                unique_uuid = uuid.uuid4()
+                if Event.objects.filter(uuid=unique_uuid).first() is not None:
+                    continue
+                self.uuid = unique_uuid
+                break
+
             self.slug = f'{slugify(self.title).lower()}-{self.uuid}'
 
         super().save(*args, **kwargs)
