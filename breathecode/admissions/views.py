426d42f2670bd0e0f094088fc92973ae56654c33
jefer94 <jdefraitaspinto@gmail.com>
2023-09-05 18:07:13 -0500

    add gevent

--- breathecode/admissions/views.py
+++ breathecode/admissions/views.py
@@ -161,8 +161,7 @@ def get_public_syllabus(request, id=None):
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def get_cohorts(request, id=None):
-    items = Cohort.objects.filter(
-        private=False).select_related('syllabus_version__syllabus').prefetch_related('cohorttimeslot_set')
+    items = Cohort.objects.filter(private=False).select_related('syllabus_version__syllabus')
 
     items = items.annotate(longitude=Value(None, output_field=FloatField()),
                            latitude=Value(None, output_field=FloatField()))
