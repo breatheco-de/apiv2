fe7251502f8a6e40b5fb648b05f53d7a466804f6
Alejandro Sanchez <aalejo@gmail.com>
2023-09-26 11:04:49 -0400

    Update celery.py

--- breathecode/celery.py
+++ breathecode/celery.py
@@ -1,39 +1,16 @@
 from __future__ import absolute_import, unicode_literals
-
 import os
 import ssl
 from celery import Celery
+from celery.signals import task_failure
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'breathecode.settings')
-REDIS_URL = os.getenv('REDIS_URL', '')
-
-# fix ssl error
-kwargs = {} if REDIS_URL.startswith('redis://') else {
-    'broker_use_ssl': {
-        'ssl_cert_reqs': ssl.CERT_NONE,
-    },
-    'redis_backend_use_ssl': {
-        'ssl_cert_reqs': ssl.CERT_NONE,
-    },
-}
-
-app = Celery('celery_breathecode', **kwargs)
-
-if os.getenv('ENV') == 'test':
-    app.conf.update(task_always_eager=True)
-
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
+	@@ -29,7 +28,12 @@
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings')
-app.conf.update(broker_url=REDIS_URL,
-                broker_pool_limit=11,
-                result_backend=REDIS_URL,
-                result_expires=10,
-                task_acks_late=True,
-                namespace='CELERY')
+app.conf.update(BROKER_URL=REDIS_URL, CELERY_RESULT_BACKEND=REDIS_URL, namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
