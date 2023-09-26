d6851934dbb6596688eb581e21d561d8b5e60cf6
jefer94 <jdefraitaspinto@gmail.com>
2023-09-26 00:04:05 -0500

    optimizing redis

--- breathecode/settings.py
+++ breathecode/settings.py
@@ -331,7 +331,8 @@ CACHES = {
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
             'CONNECTION_POOL_KWARGS': {
-                'ssl_cert_reqs': None
+                'ssl_cert_reqs': None,
+                'max_connections': 7,
             },
         }
     }
