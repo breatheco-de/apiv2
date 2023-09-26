1b6e4ff230537671eed6b20e1873fa354f3c12f5
Alejandro Sanchez <aalejo@gmail.com>
2023-09-26 11:13:03 -0400

    Update celery.py

--- breathecode/celery.py
+++ breathecode/celery.py
@@ -6,7 +6,21 @@ from celery.signals import task_failure
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'breathecode.settings')
-	@@ -29,7 +28,12 @@
+REDIS_URL = os.getenv('REDIS_URL', '')
+# fix ssl error
+kwargs = {} if REDIS_URL.startswith('redis://') else {
+    'broker_use_ssl': {
+        'ssl_cert_reqs': ssl.CERT_NONE,
+    },
+    'redis_backend_use_ssl': {
+        'ssl_cert_reqs': ssl.CERT_NONE,
+    },
+}
+app = Celery('celery_breathecode', **kwargs)
+if os.getenv('ENV') == 'test':
+    app.conf.update(task_always_eager=True)
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings')
