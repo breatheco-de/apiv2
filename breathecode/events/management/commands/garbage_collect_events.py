d23af2ed8ab1b222144cba0e6872fde8692faaf5
Alejandro Sanchez <aalejo@gmail.com>
2023-10-20 15:55:59 -0400

    Update garbage_collect_events.py

--- breathecode/events/management/commands/garbage_collect_events.py
+++ breathecode/events/management/commands/garbage_collect_events.py
@@ -8,9 +8,15 @@ class Command(BaseCommand):
     help = 'Delete logs and other garbage'
 
     def handle(self, *args, **options):
-        how_many_days = 60
+        how_many_days_with_error = 60
+        how_many_days_with_done = 30
         webhooks = EventbriteWebhook.objects.filter(created_at__lte=datetime.now() -
-                                                    timedelta(days=how_many_days))
-        count = webhooks.count()
+                                                    timedelta(days=how_many_days_with_done), status='DONE')
+        count_done = webhooks.count()
         webhooks.delete()
-        self.stdout.write(self.style.SUCCESS(f"Successfully deleted {str(count)} EventbriteWebhook's"))
+
+        webhooks = EventbriteWebhook.objects.filter(created_at__lte=datetime.now() -
+                                                    timedelta(days=how_many_days_with_error)).exclude(status='DONE')
+        count_error = webhooks.count()
+        webhooks.delete()
+        self.stdout.write(self.style.SUCCESS(f"Successfully deleted {str(count_done)} done, and {str(count_error)} errored EventbriteWebhook's"))
