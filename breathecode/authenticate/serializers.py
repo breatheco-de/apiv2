de5eca27b94a6a4b739b07b77e8ff82b423aeedc
Alejandro Sanchez <aalejo@gmail.com>
2023-10-26 17:14:57 +0000

    disabled add_activity task call from the checking enpoint

--- breathecode/authenticate/serializers.py
+++ breathecode/authenticate/serializers.py
@@ -1454,11 +1454,11 @@ class UserInviteWaitingListSerializer(serializers.ModelSerializer):
         if self.course:
             self.course.invites.add(instance)
 
-        if self.user:
-            tasks_activity.add_activity.delay(self.user.id,
-                                              'invite_created',
-                                              related_type='auth.UserInvite',
-                                              related_id=instance.id)
+        # if self.user:
+        #     tasks_activity.add_activity.delay(self.user.id,
+        #                                       'invite_created',
+        #                                       related_type='auth.UserInvite',
+        #                                       related_id=instance.id)
 
         return instance
 
