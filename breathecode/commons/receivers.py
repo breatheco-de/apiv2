f396e52708570d8cf8c3829df2ebb5a9683287c8
Alejandro Sanchez <aalejo@gmail.com>
2023-11-01 18:58:16 +0000

    deactivated cache

--- breathecode/commons/receivers.py
+++ breathecode/commons/receivers.py
@@ -7,14 +7,12 @@ import breathecode.commons.actions as actions
 
 logger = logging.getLogger(__name__)
 
+# @receiver(post_save)
+# def clean_cache_after_save(sender, **kwargs):
+#     key = hash(sender)
+#     actions.clean_cache(key)
 
-@receiver(post_save)
-def clean_cache_after_save(sender, **kwargs):
-    key = hash(sender)
-    actions.clean_cache(key)
-
-
-@receiver(post_delete)
-def clean_cache_after_delete(sender, **kwargs):
-    key = hash(sender)
-    actions.clean_cache(key)
+# @receiver(post_delete)
+# def clean_cache_after_delete(sender, **kwargs):
+#     key = hash(sender)
+#     actions.clean_cache(key)
