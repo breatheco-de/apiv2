99bf7d7c0d90f8b63062396023bff5bd89b753c4
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2023-11-13 16:44:34 -0400

    Update views.py

--- breathecode/registry/views.py
+++ breathecode/registry/views.py
@@ -29,7 +29,7 @@ from .serializers import (AssetSerializer, AssetBigSerializer, AssetMidSerialize
                           PostKeywordClusterSerializer, PostKeywordSerializer, PUTKeywordSerializer,
                           AssetKeywordBigSerializer, PUTCategorySerializer, POSTCategorySerializer,
                           KeywordClusterMidSerializer, SEOReportSerializer, OriginalityScanSerializer,
-                          VariableSmallSerializer, AssetAndTechnologySerializer)
+                          VariableSmallSerializer, AssetAndTechnologySerializer, AssetBigAndTechnologySerializer)
 from breathecode.utils import ValidationException, capable_of, GenerateLookupsMixin
 from breathecode.utils.views import render_message
 from rest_framework.response import Response
@@ -501,7 +501,7 @@ class AssetView(APIView, GenerateLookupsMixin):
             if asset is None:
                 raise ValidationException(f'Asset {asset_slug} not found', status.HTTP_404_NOT_FOUND)
 
-            serializer = AssetAndTechnologySerializer(asset)
+            serializer = AssetBigAndTechnologySerializer(asset)
             return handler.response(serializer.data)
 
         items = Asset.objects.all()
