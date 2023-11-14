42e1c07460f5a92dda448b9eb179fddadd22df27
Alejandro Sanchez <aalejo@gmail.com>
2023-11-14 17:42:25 -0500

    Update tasks.py

--- breathecode/authenticate/tasks.py
+++ breathecode/authenticate/tasks.py
@@ -26,12 +26,14 @@ def async_validate_email_invite(self, invite_id, task_manager_id):
     try:
         email_status = validate_email(user_invite.email, 'en')
         if email_status['score'] <= 0.60:
+            user_invite.status = 'REJECTED'
             user_invite.process_status = 'ERROR'
             user_invite.process_message = 'Your email is invalid'
         user_invite.email_quality = email_status['score']
         user_invite.email_status = email_status
 
     except ValidationException as e:
+        user_invite.status = 'REJECTED'
         user_invite.process_status = 'ERROR'
         user_invite.process_message = str(e)
 
