e690fedeefa3187fec5d564580173bc03ac9385e
jefer94 <jdefreitaspinto@gmail.com>
2023-11-14 01:07:11 -0500

    fix the command to allow fix the github logs

--- breathecode/authenticate/management/commands/fix_github_academy_user_logs.py
+++ breathecode/authenticate/management/commands/fix_github_academy_user_logs.py
@@ -9,14 +9,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        users = User.objects.filter(githubacademyuser__isnull=False)
+        users = User.objects.filter(githubacademyuser__isnull=False).distinct()
         academies = Academy.objects.filter()
 
         for user in users:
             for academy in academies:
                 logs = GithubAcademyUserLog.objects.filter(
-                    valid_until__isnull=True, academy_user__user=user,
-                    academy_user__academy=academy).order_by('created_at')
+                    academy_user__user=user, academy_user__academy=academy).order_by('created_at')
 
                 prev = None
                 for log in logs:
