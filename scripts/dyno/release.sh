91d62bc97232ad4ae81f28f7328318677b5f39a7
jefer94 <jdefreitaspinto@gmail.com>
2023-12-07 22:57:55 -0500

    change release script

--- scripts/dyno/release.sh
+++ scripts/dyno/release.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
 
-HIDE_CACHE_LOG=${HIDE_CACHE_LOG:-1}
-
+export HIDE_CACHE_LOG=1;
 export CORALOGIX_SUBSYSTEM=release;
+
 python manage.py migrate
 python manage.py create_academy_roles
 python manage.py set_permissions
