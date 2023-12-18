a17e5880d0f09f86977cbf287e6c0338c5ff4b20
jefer94 <jdefreitaspinto@gmail.com>
2023-12-18 14:21:18 -0500

    update migration

--- breathecode/utils/api_view_extensions/extension_base.py
+++ breathecode/utils/api_view_extensions/extension_base.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional
+from typing import Optional
 from django.db.models import QuerySet
 from django.core.handlers.wsgi import WSGIRequest
 
@@ -30,7 +30,7 @@ class ExtensionBase:
     def _set_request(self, request: WSGIRequest) -> None:
         self._request = request
 
-    def _optional_dependencies(self, **kwargs):
+    def _optional_dependencies(self, **kwargs) -> None:
         ...
 
     def __str__(self) -> str:
