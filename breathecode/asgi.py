eb1c0a6a4ce39b4b6074ad41d97af9b9a7c54a6b
jefer94 <jdefreitaspinto@gmail.com>
2024-01-12 02:12:26 -0500

    fix asgi wrapper

--- breathecode/asgi.py
+++ breathecode/asgi.py
@@ -20,4 +20,4 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'breathecode.settings')
 
 application = get_asgi_application()
-application = newrelic.agent.ASGIApplicationWrapper(application)
+application = newrelic.agent.ASGIApplicationWrapper(application=application)
