a9e3b328587afdb39457fe3c2483e07dfc6a3a77
jefer94 <jdefreitaspinto@gmail.com>
2024-01-25 09:20:56 -0500

    can unwrap the app

--- breathecode/asgi.py
+++ breathecode/asgi.py
@@ -20,4 +20,5 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'breathecode.settings')
 
 application = get_asgi_application()
-application = newrelic.agent.ASGIApplicationWrapper(application)
+if os.getenv('NOWRAP_APP') != '1':
+    application = newrelic.agent.ASGIApplicationWrapper(application)
