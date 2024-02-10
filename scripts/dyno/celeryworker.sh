4afbc90bae2aace21d48730e9c7d81fc4624833a
jefer94 <jdefreitaspinto@gmail.com>
2024-02-09 23:55:32 -0500

    generate a random pid across all dynos

--- scripts/dyno/celeryworker.sh
+++ scripts/dyno/celeryworker.sh
@@ -22,6 +22,15 @@ else
     SCALING="--concurrency=$CELERY_MAX_WORKERS"
 fi
 
+
+current_timestamp=$(date +%s)
+num_processes=$(((current_timestamp + $RANDOM) % 100))
+
+# generate a random pid
+for ((i=1; i<=$num_processes; i++)); do
+    echo "x" > /dev/null 2>&1
+done
+
 newrelic-admin run-program bin/start-pgbouncer-stunnel \
     celery -A breathecode.celery worker --loglevel=$LOG_LEVEL \
         --prefetch-multiplier=$CELERY_PREFETCH_MULTIPLIER --pool=$CELERY_POOL \
