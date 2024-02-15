e70cdc06f0dc5a13509bdc611cf91dec18ffa0cd
Alejandro Sanchez <aalejo@gmail.com>
2024-02-15 11:51:49 -0500

    Update actions.py

--- breathecode/marketing/actions.py
+++ breathecode/marketing/actions.py
@@ -536,7 +536,7 @@ def sync_automations(ac_academy):
 
     if 'automations' not in response:
         logger.error('Invalid automations incoming from AC')
-        logger.debug(response)
+        print(response)
         return False
 
     automations = response['automations']
@@ -546,7 +546,7 @@ def sync_automations(ac_academy):
         response = client.tags.list_all_tags(limit=100, offset=count)
         if 'automations' not in response:
             logger.error('Invalid automations incoming from AC pagination')
-            logger.debug(response)
+            print(response)
             return False
         automations = automations + response['automations']
 
