395190a64376b86b477509b1988d7d8307901b8f
jefer94 <jdefreitaspinto@gmail.com>
2024-03-06 12:39:47 -0500

    format

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,7 @@
 #!/bin/env python
 
 from __future__ import absolute_import
+
 import os
 import sys
 
@@ -12,7 +13,7 @@ if __name__ == '__main__':
         args = ' '.join(sys.argv)
 
     # exit_code = os.system(f'pytest {args} --disable-pytest-warnings --nomigrations --durations=1')
-    exit_code = os.system(f'pytest {args} --nomigrations --durations=1')
+    exit_code = os.system(f'pytest {args} --durations=1')
 
     # python don't return 256
     if exit_code:
