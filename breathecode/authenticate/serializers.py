e5ec2851f8fce923d6e1fb03ae5986d8251b1009
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2024-03-26 15:08:05 -0400

    Update serializers.py

--- breathecode/authenticate/serializers.py
+++ breathecode/authenticate/serializers.py
@@ -1406,7 +1406,7 @@ class UserInviteWaitingListSerializer(serializers.ModelSerializer):
                                           code=400)
 
             expected_keys = [
-                'utm_placement', 'utm_medium', 'utm_source', 'utm_term', 'utm_content', 'conversion_url', 'landing_url',
+                'utm_placement', 'utm_medium', 'utm_source', 'utm_term', 'utm_content', 'utm_campaign', 'conversion_url', 'landing_url',
                 'user_agent', 'plan', 'location', 'internal_cta_placement', 'internal_cta_content',
                 'internal_cta_campaign'
             ]
