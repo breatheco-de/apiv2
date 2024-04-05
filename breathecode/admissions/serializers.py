aaa4e592de054d9a2db9121223ec89023dbe7818
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2024-04-05 10:36:35 -0400

    Update serializers.py

--- breathecode/admissions/serializers.py
+++ breathecode/admissions/serializers.py
@@ -725,6 +725,7 @@ class CohortPUTSerializer(CohortSerializerMixin):
     stage = serializers.CharField(required=False)
     language = serializers.CharField(required=False)
     is_hidden_on_prework = serializers.BooleanField(required=False, allow_null=True)
+    never_ends = serializers.BooleanField(required=False, allow_null=True)
 
     class Meta:
         model = Cohort
