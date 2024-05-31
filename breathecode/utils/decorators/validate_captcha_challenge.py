a591a5ab4f11ac0d6f2d421c118f0ce479c28f01
Alejandro Sanchez <aalejo@gmail.com>
2024-05-31 11:53:19 -0400

    Update validate_captcha_challenge.py

--- breathecode/utils/decorators/validate_captcha_challenge.py
+++ breathecode/utils/decorators/validate_captcha_challenge.py
@@ -29,9 +29,9 @@ def validate_captcha_challenge(function):
             else:
                 raise IndexError()
 
-            apply_captcha = os.getenv('APPLY_CAPTCHA', False)
+            apply_captcha = os.getenv('APPLY_CAPTCHA', 'FALSE')
 
-            if not apply_captcha:
+            if not apply_captcha or apply_captcha == 'FALSE':
                 return function(*args, **kwargs)
 
             logger.info('VERIFYING THE CAPTCHA')
