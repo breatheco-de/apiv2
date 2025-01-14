41a243a49977f7f633061bbbd3b290fed999e4bf
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2025-01-14 13:09:06 -0400

    Update batch.py

--- breathecode/services/learnpack/actions/batch.py
+++ breathecode/services/learnpack/actions/batch.py
@@ -3,21 +3,22 @@ import logging
 logger = logging.getLogger(__name__)
 from breathecode.assignments.models import AssignmentTelemetry, LearnPackWebhook
 
-
 def batch(self, webhook: LearnPackWebhook):
     # lazyload to fix circular import
+    from breathecode.registry.models import Asset
     from breathecode.assignments.models import Task
 
     _slug = webhook.payload["slug"]
+    asset = Asset.get_by_slug(_slug)
 
-    telemetry = AssignmentTelemetry.objects.filter(asset_slug=_slug, user__id=webhook.payload["user_id"]).first()
+    telemetry = AssignmentTelemetry.objects.filter(asset_slug=asset.slug, user__id=webhook.payload["user_id"]).first()
 
-    assets = Task.objects.filter(associated_slug=_slug, user__id=webhook.student.id)
+    assets = Task.objects.filter(associated_slug=asset.slug, user__id=webhook.student.id)
     if assets.count() == 0:
         raise Exception(f"Student with id {webhook.student.id} has not tasks with associated slug {_slug}")
 
     if telemetry is None:
-        telemetry = AssignmentTelemetry(user=webhook.student, asset_slug=_slug, telemetry=webhook.payload)
+        telemetry = AssignmentTelemetry(user=webhook.student, asset_slug=asset.slug, telemetry=webhook.payload)
         telemetry.save()
         # All assets with the same associated slug should share the same telemetry
         for a in assets:
