98cc1ce205422e60fa81c5bfba82e7b8403ca5dc
Angie Vanegas P <arco527@gmail.com>
2025-01-27 22:25:07 +0000

    Add recording_url in backend

--- breathecode/services/eventbrite/actions/event_created.py
+++ breathecode/services/eventbrite/actions/event_created.py
@@ -6,8 +6,8 @@ logger = logging.getLogger(__name__)
 
 def event_created(self, webhook, payload: dict):
     # lazyload to fix circular import
-    from breathecode.events.models import Organization
     from breathecode.events.actions import update_or_create_event
+    from breathecode.events.models import Organization
 
     org = Organization.objects.filter(id=webhook.organization_id).first()
 
