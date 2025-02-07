a97e353e53497d5255574e0ace82758e85c3463b
jefer94 <jdefreitaspinto@gmail.com>
2025-02-06 21:26:07 -0500

    add flower

---
+++ scripts/dyno/flower.sh
@@ -0,0 +1,39 @@
+#!/bin/env bash
+
+CELERY_MIN_WORKERS=${CELERY_MIN_WORKERS:-2}
+CELERY_MAX_WORKERS=${CELERY_MAX_WORKERS:-2}
+CELERY_PREFETCH_MULTIPLIER=${CELERY_PREFETCH_MULTIPLIER:-2}
+
+CELERY_POOL=${CELERY_POOL:-prefork}
+LOG_LEVEL=${LOG_LEVEL:-INFO}
+
+export NEW_RELIC_METADATA_COMMIT=$HEROKU_SLUG_COMMIT;
+export CORALOGIX_SUBSYSTEM=celeryworker;
+export CELERY_WORKER_RUNNING=True;
+export REMAP_SIGTERM=SIGQUIT;
+export CELERY_POOL=$CELERY_POOL;
+
+# it will be used by upload_activities
+export CELERY_MAX_WORKERS=$CELERY_MAX_WORKERS;
+
+if [ "$CELERY_POOL" == "gevent" ] || [ "$CELERY_POOL" == "prefork" ]; then
+    SCALING="--autoscale=$CELERY_MIN_WORKERS,$CELERY_MAX_WORKERS"
+else
+    SCALING="--concurrency=$CELERY_MAX_WORKERS"
+fi
+
+
+current_timestamp=$(date +%s)
+num_processes=$(((current_timestamp + $RANDOM) % 100))
+
+# generate a random pid
+for ((i=1; i<=$num_processes; i++)); do
+    true &
+done
+
+wait
+
+newrelic-admin run-program bin/start-pgbouncer \
+    python -m celery -A breathecode.celery flower --port=$PORT --loglevel=$LOG_LEVEL \
+        --prefetch-multiplier=$CELERY_PREFETCH_MULTIPLIER --pool=$CELERY_POOL \
+        $SCALING
