d062051f20ff074b5f27f91c96c0963f0533eb7a
Alejandro Sanchez <aalejo@gmail.com>
2025-02-07 06:19:19 +0000

    pagination in garbage collector for webhooks

--- breathecode/assignments/management/commands/assignments_garbage_collect.py
+++ breathecode/assignments/management/commands/assignments_garbage_collect.py
@@ -1,17 +1,23 @@
 from django.core.management.base import BaseCommand
-from django.db import connection
+from django.utils import timezone
+from datetime import timedelta
+from assignments.models import LearnPackWebhook
 
 
 class Command(BaseCommand):
     help = "Clean data from marketing module"
 
     def handle(self, *args, **options):
-
         self.delete_old_webhooks()
 
-    def delete_old_webhooks(self):
-        cursor = connection.cursor()
-        # status = 'ERROR' or status = 'PENDING' AND
-        cursor.execute("DELETE FROM assignments_learnpackwebhook WHERE created_at < NOW() - INTERVAL '30 days'")
+    def delete_old_webhooks(self, batch_size=1000):
+        thirty_days_ago = timezone.now() - timedelta(days=30)
+        while True:
+            old_webhooks = LearnPackWebhook.objects.filter(created_at__lt=thirty_days_ago).exclude(
+                status__in=["ERROR", "PENDING"]
+            )[:batch_size]
 
-        cursor.execute("DELETE FROM assignments_learnpackwebhook WHERE status <> 'ERROR' AND status <> 'PENDING'")
+            if not old_webhooks.exists():
+                break
+
+            old_webhooks.delete()
