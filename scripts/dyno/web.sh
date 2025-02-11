89247cc4eb76b25dfe507745ad5e13d4c08c41a7
jefer94 <jdefreitaspinto@gmail.com>
2025-02-10 19:02:05 -0500

    remove unused node app from web dyno

--- scripts/dyno/web.sh
+++ scripts/dyno/web.sh
@@ -33,18 +33,18 @@ fi
     gunicorn breathecode.$SERVER_TYPE --timeout $WEB_TIMEOUT --workers $WEB_WORKERS \
         --worker-connections $WEB_WORKER_CONNECTION --worker-class $WEB_WORKER_CLASS \
         --max-requests $WEB_MAX_REQUESTS --max-requests-jitter $WEB_MAX_REQUESTS_JITTER \
-        $GUNICORN_PARAMS &
+        $GUNICORN_PARAMS
 
-pid1=$!
+# pid1=$!
 
-if [ "$SSR" = "1" ]; then
-    curl -fsSL https://bun.sh/install | bash
-    export BUN_INSTALL="$HOME/.bun"
-    export PATH=$BUN_INSTALL/bin:$PATH
+# if [ "$SSR" = "1" ]; then
+#     curl -fsSL https://bun.sh/install | bash
+#     export BUN_INSTALL="$HOME/.bun"
+#     export PATH=$BUN_INSTALL/bin:$PATH
 
-    newrelic-admin run-program bun ssr.ts &
-    pid2=$!
-    wait $pid2
-fi
+#     newrelic-admin run-program bun ssr.ts &
+#     pid2=$!
+#     wait $pid2
+# fi
 
-wait $pid1
+# wait $pid1
