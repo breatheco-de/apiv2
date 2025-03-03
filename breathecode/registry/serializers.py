b042ff602c6827f57cd920c2dc72c87c6e7ab134
Luis Miguel Del Valle <informacionlmdv@gmail.com>
2025-03-03 19:56:28 +0000

    adding dependencies

--- breathecode/registry/serializers.py
+++ breathecode/registry/serializers.py
@@ -472,7 +472,7 @@ class AssetExpandableSerializer(AssetMidSerializer):
                     elem["technologies"] = self.format_technologies(obj)
 
                 elem["template_url"] = obj.template_url if hasattr(obj, "template_url") else None
-                elem["dependencies"] = obj.template_url if hasattr(obj, "template_url") else None
+                elem["dependencies"] = obj.dependencies if hasattr(obj, "dependencies") else None
 
                 if "readme" in self.expand:
                     url = obj.readme_url
