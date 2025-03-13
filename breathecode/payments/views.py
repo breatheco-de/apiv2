df1a2dd696877bc0e80816a429e32c2037c43905
Angie Vanegas P <arco527@gmail.com>
2025-03-13 16:03:28 +0000

    Refact code

--- breathecode/payments/views.py
+++ breathecode/payments/views.py
@@ -967,7 +967,6 @@ class AcademySubscriptionView(APIView):
 
     extensions = APIViewExtensions(sort="-id", paginate=True)
 
-    @capable_of("read_subscription")
     def get(self, request, subscription_id=None, academy_id=None):
         handler = self.extensions(request)
         lang = get_user_language(request)
