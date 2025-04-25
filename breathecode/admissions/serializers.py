569990ac15defca70da0fe5e94f40c83c3acdc05
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2025-04-25 15:53:49 +0000

    Add updated_at to cohort user serializers

--- breathecode/admissions/serializers.py
+++ breathecode/admissions/serializers.py
@@ -481,6 +481,7 @@ class CohortUserHookSerializer(serpy.Serializer):
     educational_status = serpy.Field()
     watching = serpy.Field()
     created_at = serpy.Field()
+    updated_at = serpy.Field()
     profile_academy = serpy.MethodField()
 
     def get_profile_academy(self, obj):
@@ -522,6 +523,7 @@ class GetCohortUserSerializer(serpy.Serializer):
     educational_status = serpy.Field()
     watching = serpy.Field()
     created_at = serpy.Field()
+    updated_at = serpy.Field()
     profile_academy = serpy.MethodField()
 
     def get_profile_academy(self, obj):
