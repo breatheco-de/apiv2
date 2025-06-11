64a2e418aefc95c5189ee41c678266f8ef7b8d71
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2025-06-11 14:54:35 -0400

    Update general_pricing_ratios.py

--- settings/general_pricing_ratios.py
+++ settings/general_pricing_ratios.py
@@ -7,18 +7,18 @@ class PricingRatio(TypedDict):
 
 
 GENERAL_PRICING_RATIOS: Dict[str, PricingRatio] = {
-    "ve": {"pricing_ratio": 0.8, "currency": "USD"},
+    "ve": {"pricing_ratio": 1.0, "currency": "USD"},
     "mx": {"pricing_ratio": 1.0, "currency": "USD"},
     "us": {"pricing_ratio": 1.0, "currency": "USD"},
-    "co": {"pricing_ratio": 0.9, "currency": "USD"},
-    "cl": {"pricing_ratio": 0.95, "currency": "USD"},
-    "ar": {"pricing_ratio": 0.8, "currency": "USD"},
-    "pe": {"pricing_ratio": 0.85, "currency": "USD"},
-    "ec": {"pricing_ratio": 0.8, "currency": "USD"},
-    "do": {"pricing_ratio": 0.9, "currency": "USD"},
-    "cr": {"pricing_ratio": 0.9, "currency": "USD"},
-    "pa": {"pricing_ratio": 0.95, "currency": "USD"},
-    "uy": {"pricing_ratio": 0.9, "currency": "USD"},
+    "co": {"pricing_ratio": 1.0, "currency": "USD"},
+    "cl": {"pricing_ratio": 1.0, "currency": "USD"},
+    "ar": {"pricing_ratio": 1.0, "currency": "USD"},
+    "pe": {"pricing_ratio": 1.0, "currency": "USD"},
+    "ec": {"pricing_ratio": 1.0, "currency": "USD"},
+    "do": {"pricing_ratio": 1.0, "currency": "USD"},
+    "cr": {"pricing_ratio": 1.0, "currency": "USD"},
+    "pa": {"pricing_ratio": 1.0, "currency": "USD"},
+    "uy": {"pricing_ratio": 1.0, "currency": "USD"},
     "br": {"pricing_ratio": 1.0, "currency": "USD"},
     "es": {"pricing_ratio": 1.0, "currency": "EUR"},
 }
