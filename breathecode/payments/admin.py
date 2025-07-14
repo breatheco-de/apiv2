f572cf0ce77d276b3716d1b9f6c7c1002e586027
Alejandro Sanchez <aalejo@gmail.com>
2025-07-14 13:50:31 -0400

    Update admin.py

--- breathecode/payments/admin.py
+++ breathecode/payments/admin.py
@@ -100,7 +100,7 @@ class FinancingOptionAdmin(admin.ModelAdmin):
 class PlanAdmin(admin.ModelAdmin):
     list_display = ("id", "slug", "status", "trial_duration", "trial_duration_unit", "owner")
     list_filter = ["trial_duration_unit", "owner"]
-    search_fields = ["lang", "title"]
+    search_fields = ["title"]
     raw_id_fields = ["owner"]
     filter_horizontal = ("invites",)
 
