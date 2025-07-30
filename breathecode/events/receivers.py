1e7fe1bc84f3e57f5dfa9bee766013edea4a9ad6
Aleperix <aleperixx@gmail.com>
2025-07-30 22:01:08 +0000

    feat(events): add live_workshop_status endpoint with cache invalidation

--- breathecode/events/receivers.py
+++ breathecode/events/receivers.py
@@ -1,15 +1,16 @@
 import logging
 from typing import Any, Type
 
+from django.core.cache import cache
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
 from django.utils import timezone
 
 from breathecode.admissions.models import CohortTimeSlot
 from breathecode.admissions.signals import timeslot_saved
 from breathecode.events import tasks
-from breathecode.events.models import Event, FINISHED
-from breathecode.events.signals import event_status_updated
-
+from breathecode.events.models import FINISHED, Event
+from breathecode.events.signals import event_saved, event_status_updated
 
 logger = logging.getLogger(__name__)
 
@@ -37,3 +38,13 @@ def generate_event_recap_on_finished(sender: Type[Event], instance: Event, **kwa
     if instance.status == FINISHED:
         logger.info(f"Event {instance.id} marked as FINISHED, queuing recap generation")
         tasks.generate_event_recap.delay(instance.id)
+
+
+@receiver(event_saved)
+def invalidate_cache_on_event_saved(sender, instance, **kwargs):
+    cache.delete("live_workshop_status")
+
+
+@receiver(post_delete, sender=Event)
+def invalidate_cache_on_event_deleted(sender, instance, **kwargs):
+    cache.delete("live_workshop_status")
