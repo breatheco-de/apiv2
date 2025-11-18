a928913c2c38870e4369286e600763d548a18aaf
alejandrojmnzz <alejandrojmnzz@gmail.com>
2025-11-18 19:39:42 +0000

    .delay changed to .send_robust on signal for mentorship_session_status

--- breathecode/mentorship/models.py
+++ breathecode/mentorship/models.py
@@ -470,10 +470,10 @@ class MentorshipSession(models.Model):
         super().save(*args, **kwargs)
 
         if is_creating or self.__old_status != self.status:
-            signals.mentorship_session_status.delay(instance=self, sender=MentorshipSession)
+            signals.mentorship_session_status.send_robust(instance=self, sender=MentorshipSession)
 
         # we're testing why there hasn't any mentorship session with a survey changing mentorship_session_status by mentorship_session_saved
-        signals.mentorship_session_saved.delay(instance=self, sender=MentorshipSession)
+        signals.mentorship_session_saved.send_robust(instance=self, sender=MentorshipSession)
 
         self.__old_status = self.status
 
