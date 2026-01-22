d16edf53d343e33ceb70cfa5a265405d75c0d9e4
unknown <informacionlmdv@gmail.com>
2026-01-21 21:50:13 -0400

    making some changes to allow autoignore proyects on an academy

--- breathecode/assignments/serializers.py
+++ breathecode/assignments/serializers.py
@@ -362,10 +362,38 @@ class PUTTaskSerializer(serializers.ModelSerializer):
                 related_id=instance.id,
             )
 
+        # Auto-ignore projects on delivery if feature flag is enabled
         if (
             "task_status" in validated_data
             and validated_data["task_status"] == "DONE"
-            and validated_data["revision_status"] != "APPROVED"
+            and instance.task_status != "DONE"
+            and instance.task_type == "PROJECT"
+            and instance.cohort
+            and instance.user.id == self.context["request"].user.id
+        ):
+            from breathecode.admissions.utils.academy_features import has_feature_flag
+            from django.utils import timezone
+
+            revision_status_set = "revision_status" in validated_data
+            revision_status_value = validated_data.get("revision_status")
+            
+            should_auto_ignore = (
+                not revision_status_set or revision_status_value in (None, "PENDING", "")
+            )
+            
+            if should_auto_ignore:
+                auto_ignore_enabled = has_feature_flag(
+                    instance.cohort.academy, "certificate.auto_ignore_projects_on_delivery", default=False
+                )
+                
+                if auto_ignore_enabled:
+                    validated_data["revision_status"] = "IGNORED"
+                    validated_data["reviewed_at"] = timezone.now()
+
+        if (
+            "task_status" in validated_data
+            and validated_data["task_status"] == "DONE"
+            and validated_data.get("revision_status") != "APPROVED"
             and "flags" in validated_data
         ):
             print(validated_data)
@@ -373,7 +401,9 @@ class PUTTaskSerializer(serializers.ModelSerializer):
 
             async_validate_flags.delay(instance.id, instance.associated_slug, validated_data["flags"])
 
-        return super().update(instance, validated_data)
+        result = super().update(instance, validated_data)
+        
+        return result
 
 
 class FinalProjectGETSerializer(serpy.Serializer):
