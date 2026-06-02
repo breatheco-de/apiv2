136f5899e0cb6bb9fe8bd724e0d0a9cdc38c6d1a
unknown <informacionlmdv@gmail.com>
2026-06-02 12:41:35 -0400

    small bug fix

--- breathecode/events/migrations/0007_eventcheckin_utm_location.py
+++ breathecode/events/migrations/0007_eventcheckin_utm_location.py
@@ -14,35 +14,3 @@ class Migration(migrations.Migration):
             field=models.CharField(blank=True, default=None, max_length=70, null=True),
         ),
     ]
-from django.db import migrations, models
-
-
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ("events", "0006_academyeventsettings"),
-    ]
-
-    operations = [
-        migrations.AddField(
-            model_name="eventcheckin",
-            name="utm_location",
-            field=models.CharField(blank=True, default=None, max_length=70, null=True),
-        ),
-    ]
-from django.db import migrations, models
-
-
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ("events", "0006_academyeventsettings"),
-    ]
-
-    operations = [
-        migrations.AddField(
-            model_name="eventcheckin",
-            name="utm_location",
-            field=models.CharField(blank=True, default=None, max_length=70, null=True),
-        ),
-    ]
