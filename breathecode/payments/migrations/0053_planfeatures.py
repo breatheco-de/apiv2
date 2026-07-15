5c1a203223e838c68e44776302edd41f137bcc9f
lumi-tip <informacionlmdv@gmail.com>
2026-07-15 14:38:04 -0400

    solve migration error

--- breathecode/payments/migrations/0053_planfeatures.py
+++ breathecode/payments/migrations/0053_planfeatures.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name="PlanFeatures",
             fields=[
-                ("id", models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
                 (
                     "bullets",
                     models.JSONField(
